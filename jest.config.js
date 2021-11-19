66001c3660fed27fb6236abedc8dfb0df8fa523e
chenlongbo <chenlongbo@illasoft.com>
2021-11-19 23:46:30 +0800

    add circle ci

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   testEnvironment: "jsdom",
+  coverageDirectory: "./coverage/",
+  collectCoverage: true,
   collectCoverageFrom: ["packages/**/*.{ts,tsx}"],
   moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
   extensionsToTreatAsEsm: [".ts"],
