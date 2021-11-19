c8694c15711dd69c6e378d1dc74d225580625f20
chenlongbo <chenlongbo@illasoft.com>
2021-11-19 14:17:59 +0800

    add a11y and test

--- .storybook/main.js
+++ .storybook/main.js
@@ -8,6 +8,7 @@ module.exports = {
     "@storybook/addon-essentials",
     "@storybook/addon-jest",
     "@storybook/addon-storysource",
+    "@storybook/addon-a11y",
     "storybook-zeplin/register",
   ],
   "framework": "@storybook/react",
---
+++ packages/tag/tests/tag.test.tsx
@@ -0,0 +1,10 @@
+/** @jsxImportSource @emotion/react */
+import * as React from "react"
+
+test("Tag css check", () => {
+
+})
+
+test("Tag with close button renders correctly", () => {
+
+})
