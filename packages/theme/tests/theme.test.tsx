ecd12c0173a261bdb9b32590dd4b75a74d4d4fc2
smallSohoSolo <chenlongbo@illasoft.com>
2021-11-23 18:18:20 +0800

    add system theme jest case

--- packages/theme/tests/theme.test.tsx
+++ packages/theme/tests/theme.test.tsx
@@ -1,5 +1,7 @@
 import { globalColor } from "../src"
+import { illaPrefix } from "../dist/types"
 
-test("Theme get null colors", () => {
+test("Theme get color", () => {
   expect(globalColor("null")).toBe("")
+  expect(globalColor(`--${illaPrefix}-green-01`)).toBe("#007a41")
 })
