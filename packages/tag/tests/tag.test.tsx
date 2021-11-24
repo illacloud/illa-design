e4f90f23a947ab3c681b8885c1f54d08f617904e
smallSohoSolo <chenlongbo@illasoft.com>
2021-11-24 15:47:00 +0800

    update tag test case

--- packages/tag/tests/tag.test.tsx
+++ packages/tag/tests/tag.test.tsx
@@ -1,36 +1,47 @@
 import { Tag } from "../src"
 import { create } from "react-test-renderer"
 import { BsFacebook } from "react-icons/bs"
+import { TagColorScheme, TagVariant } from "../dist/types"
 
 test("Tag renders with text", () => {
-  const node = create(<Tag className="test-with-text">Tag renders with text</Tag>)
-  expect(node.root.findByType(Tag).props.className).toBe("test-with-text")
+  const { root } = create(<Tag className="test-with-text">Tag renders with text</Tag>)
+  expect(root.findByType(Tag).props.className).toBe("test-with-text")
 })
 
 test("Tag renders with icon", () => {
-  const node = create(<Tag className="test-with-icon" icon={<BsFacebook />} />)
-  expect(node.root.findByType(Tag).props.className).toBe("test-with-icon")
+  const { root } = create(<Tag className="test-with-icon" icon={<BsFacebook />} />)
+  expect(root.findByType(Tag).props.className).toBe("test-with-icon")
 })
 
 test("Tag renders invisible", () => {
-  const node = create(
+  const { root } = create(
     <Tag className="test-invisible" visible={false} />,
   )
-  expect(node.root.findByType(Tag).instance).toBe(null)
+  expect(root.findByType(Tag).props.className).toBe("test-invisible")
+})
+
+test("Tag renders with color and variant", () => {
+  let variantList: TagVariant[] = ["light", "fill", "outline"]
+  let colorList: TagColorScheme[] = ["gray", "green", "#123321"]
+  for (let v of variantList) {
+    for (let c of colorList) {
+      expect(create(<Tag colorScheme={c} variant={v}>Hello World</Tag>).toJSON()).toMatchSnapshot()
+    }
+  }
 })
 
 test("Tag renders with close icon", () => {
-  const node = create(
+  const { root } = create(
     <Tag className="test-with-close-icon" closable />,
   )
-  expect(node.root.findByType(Tag).props.className).toBe("test-with-close-icon")
+  expect(root.findByType(Tag).props.className).toBe("test-with-close-icon")
 })
 
 test("Tag renders close event", () => {
   const onClose = jest.fn()
   const node = create(
-    <Tag className="test-invisible" visible={false} onClose={onClose} />,
+    <Tag className="test-close-event" onClose={onClose} closable />,
   )
-  node.root.findByProps({ className: "test-invisible" }).props.onClose()
+  node.root.findByProps({ className: "test-close-event" }).props.onClose()
   expect(onClose).toBeCalled()
 })
