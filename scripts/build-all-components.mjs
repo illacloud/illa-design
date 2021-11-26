55fa5815c7ab964b1960040b315bd923e59cafa1
smallSohoSolo <b3069741@gmail.com>
2021-11-26 16:28:30 +0800

    register space

--- scripts/build-all-components.mjs
+++ scripts/build-all-components.mjs
@@ -1,7 +1,7 @@
 #!/usr/bin/env zx
 
 // note alignment
-const components = ["system", "theme", "icon", "image", "tag", "avatar", "divider"]
+const components = ["system", "theme", "icon", "image", "tag", "avatar", "divider", "space"]
 
 for (let i = 0; i < components.length; i++) {
   await $`npx lerna run build --scope=@illa-design/${components[i]}`
