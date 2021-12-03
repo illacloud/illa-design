5d6cca9aa25642667fe03e5ecab0776b49549901
smallSohoSolo <b3069741@gmail.com>
2021-12-03 19:33:30 +0800

    add button & button group test cases

--- packages/theme/tests/theme.test.tsx
+++ packages/theme/tests/theme.test.tsx
@@ -4,4 +4,5 @@ import { illaPrefix } from "../src"
 test("Theme get color", () => {
   expect(globalColor("null")).toBe("")
   expect(globalColor(`--${illaPrefix}-green-01`)).toBe("#007a41")
+  expect(globalColor("transparent")).toBe("transparent")
 })
