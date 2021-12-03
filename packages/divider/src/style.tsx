0ba424700b3f44a02115a1f81c362aaecee2cb0a
smallSohoSolo <b3069741@gmail.com>
2021-12-03 18:17:52 +0800

    divide style

---
+++ packages/divider/src/style.tsx
@@ -0,0 +1,26 @@
+import { DividerVariant } from "./interface"
+import { css, SerializedStyles } from "@emotion/react"
+import { globalColor, illaPrefix } from "@illa-design/theme"
+
+export function applyDividerContainerHorizontal(variant: DividerVariant): SerializedStyles {
+  return css`
+    display: inline-flex;
+    vertical-align: middle;
+    border-color: ${globalColor(`--${illaPrefix}-gray-08`)};
+    border-style: ${variant};
+    border-width: 0 0 ${variant == "double" ? "3px" : "1px"} 0;
+    width: 100%;
+  `
+}
+
+export function applyDividerContainerVertical(variant: DividerVariant): SerializedStyles {
+  return css`
+    display: inline-flex;
+    vertical-align: middle;
+    border-width: 0 0 0 ${variant == "double" ? "3px" : "1px"};
+    border-image: initial;
+    border-color: ${globalColor(`--${illaPrefix}-gray-08`)};
+    border-style: ${variant};
+    height: 1em;
+  `
+}
---
+++ packages/image/src/style.tsx
@@ -0,0 +1,28 @@
+import { css } from "@emotion/core"
+import { globalColor, illaPrefix } from "@illa-design/theme"
+
+export function applyImageCss(objFit: "fill" | "container" | "cover" | "none" | "scale-down", radius: string) {
+  return css`
+    object-fit: ${objFit};
+    background-color: ${globalColor(`--${illaPrefix}-blackAlpha-09`)};
+    border-radius: ${radius};
+  `
+}
+
+export function applyDefaultFallback(width: string | number, height: string | number, radius: string) {
+  return css`
+    display: inline-flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    background-color: ${globalColor(`--${illaPrefix}-blackAlpha-09`)};
+    border-radius: ${radius};
+    width: ${width};
+    height: ${height};
+  `
+}
+
+export const applyOuterCss = css`
+  vertical-align:middle;
+  display: inline-flex;
+`
