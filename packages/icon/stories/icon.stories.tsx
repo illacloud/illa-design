657355e70e001664065c8b407e7d35a428f01cb9
smallSohoSolo <b3069741@gmail.com>
2021-12-17 15:46:48 +0800

    fix: default icon bug

--- packages/icon/stories/icon.stories.tsx
+++ packages/icon/stories/icon.stories.tsx
@@ -1,6 +1,6 @@
 import * as React from "react"
 import { Meta, Story } from "@storybook/react"
-import { CloseIcon, CopyIcon, IconProps, LoadingIcon, PersonIcon } from "../src"
+import { CloseIcon, CopyIcon, IconProps, ImageDefaultIcon, LoadingIcon, PersonIcon } from "../src"
 import results from "../../../coverage/coverage-final.json"
 import { withTests } from "@storybook/addon-jest"
 
@@ -23,8 +23,9 @@ export default {
 } as Meta
 
 export const Close: Story<IconProps> = (props) => <CloseIcon {...props} />
-export const Person: Story<IconProps> = (props) => <PersonIcon {...props} />
-export const Loading: Story<IconProps> = (props) => <LoadingIcon {...props} />
 export const Copy: Story<IconProps> = (props) => <CopyIcon {...props} />
+export const ImageDefault: Story<IconProps> = (props) => <ImageDefaultIcon {...props} />
+export const Loading: Story<IconProps> = (props) => <LoadingIcon {...props} />
+export const Person: Story<IconProps> = (props) => <PersonIcon {...props} />
 
 
