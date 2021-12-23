9a6fd4433348e5d80a0c40dd7e77bc21b3a4a57b
smallSohoSolo <b3069741@gmail.com>
2021-12-23 19:31:48 +0800

    chore: change trigger story

--- packages/trigger/stories/trigger.stories.tsx
+++ packages/trigger/stories/trigger.stories.tsx
@@ -14,9 +14,33 @@ export default {
 } as Meta
 
 const T: Story<TriggerProps> = (args) => {
-  return <Space style={{padding: "200px"}}>
-    <Trigger {...args}><Button>Hover Here</Button></Trigger>
-    <Button>Second</Button>
+  return <Space style={{ padding: "200px" }} direction="vertical">
+
+    <Space style={{ marginLeft: "70px" }}>
+      <Trigger {...args} position="tl"><Button>TL</Button></Trigger>
+      <Trigger {...args} position="top"><Button>TOP</Button></Trigger>
+      <Trigger {...args} position="tr"><Button>TR</Button></Trigger>
+    </Space>
+
+    <Space direction="horizontal" size="200px">
+      <Space direction="vertical">
+        <Trigger {...args} position="lt"><Button>LT</Button></Trigger>
+        <Trigger {...args} position="left"><Button>LEFT</Button></Trigger>
+        <Trigger {...args} position="lb"><Button>LB</Button></Trigger>
+      </Space>
+      <Space direction="vertical">
+        <Trigger {...args} position="rt"><Button>RT</Button></Trigger>
+        <Trigger {...args} position="right"><Button>RIGHT</Button></Trigger>
+        <Trigger {...args} position="rb"><Button>TR</Button></Trigger>
+      </Space>
+    </Space>
+
+    <Space style={{ marginLeft: "70px" }}>
+      <Trigger {...args} position="bl"><Button>BL</Button></Trigger>
+      <Trigger {...args} position="bottom"><Button>BOTTOM</Button></Trigger>
+      <Trigger {...args} position="br"><Button>BR</Button></Trigger>
+    </Space>
+
   </Space>
 }
 
