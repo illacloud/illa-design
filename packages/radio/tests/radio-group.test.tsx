176dd8e5c1c15f3c68cf757b7c46b39c949198d2
xiaoyu-a <lavamint@outlook.com>
2022-01-05 15:34:45 +0800

    feat: add radio event test

--- packages/radio/tests/radio-group.test.tsx
+++ packages/radio/tests/radio-group.test.tsx
@@ -1,25 +1,27 @@
-import { Radio, RadioGroup } from "../src"
-import { render, screen } from "@testing-library/react"
-import "@testing-library/jest-dom"
 import * as React from "react"
+import { Radio, RadioGroup } from "../src"
+import { fireEvent, render, screen, waitFor } from "@testing-library/react"
+import "@testing-library/jest-dom"
 
 test("RadioGroup renders correctly", () => {
-  render(
-    <RadioGroup data-testid="test-radio-group" aria-label="radio-group">
+  const { asFragment } = render(
+    <RadioGroup data-testid="test-radio-group" aria-label="radio-group" role={"group"}>
       <Radio value="a">a</Radio>
       <Radio value="b">b</Radio>
       <Radio value="c">c</Radio>
     </RadioGroup>,
   )
-  // expect(screen.getByLabelText("radio-group")).toBeInTheDocument()
+  // expect(asFragment()).toMatchSnapshot()
+  // expect(screen.getAllByRole("group")).toBeInTheDocument()
+  // expect(screen.getAllByRole("radio-group")).toBeInTheDocument()
 })
 
 test("RadioGroup options renders correctly", () => {
   render(
-    <RadioGroup options={["A", "B", "C"]} aria-label="radio-group-with-options">
+    <RadioGroup options={["A", "B", "C"]} data-testid="test-radio-group-1">
     </RadioGroup>,
   )
-  // expect(screen.getByLabelText("radio-group-with-options")).toBeInTheDocument()
+  // expect(screen.getByTestId("test-radio-group-1")).toBeInTheDocument()
 })
 
 test("RadioGroup renders with value", () => {
@@ -28,4 +30,17 @@ test("RadioGroup renders with value", () => {
     </RadioGroup>,
   )
   expect(screen.getByLabelText("valueA")).toBeChecked()
+})
+
+test("RadioGroup renders with click", async () => {
+  render(
+    <RadioGroup options={["clickA", "clickB", "clickC"]}>
+    </RadioGroup>,
+  )
+  fireEvent.click(screen.getByLabelText("clickB"))
+  await waitFor(() => {
+    expect(screen.getByLabelText("clickB")).toBeChecked()
+  }, {
+    timeout: 3000,
+  })
 })
