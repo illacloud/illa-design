7a5670274ebfa44fb7dc6d4c1a4a33b49f56f528
xiaoyu-a <lavamint@outlook.com>
2022-01-07 14:44:24 +0800

    feat: fix radio.stories.tsx

--- packages/radio/stories/radio.stories.tsx
+++ packages/radio/stories/radio.stories.tsx
@@ -10,11 +10,6 @@ export default {
   title: "DATA INPUT/Radio",
   component: Radio,
   decorators: [withTests({ results })],
-  argTypes: {
-    icon: {
-      control: false,
-    }
-  }
 } as Meta
 
 const Template: Story<RadioProps> = (args) => <Radio {...args}>ILLA</Radio>
