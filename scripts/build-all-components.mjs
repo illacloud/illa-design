71c46dd65ff8449b913ef3c70f840f7764dcfdc1
smallSohoSolo <b3069741@gmail.com>
2022-01-27 18:32:52 +0800

    fix: progress depenencies

--- scripts/build-all-components.mjs
+++ scripts/build-all-components.mjs
@@ -1,7 +1,7 @@
 #!/usr/bin/env zx
 
 // note alignment
-const components = ["system", "config-provider", "theme", "icon", "progress", "link", "image", "tag", "avatar", "divider", "radio", "space", "button", "trigger", "tooltip", "typography", "popover", "input", "empty"]
+const components = ["system", "config-provider", "theme", "icon", "link", "image", "tag", "avatar", "divider", "radio", "space", "button", "trigger", "progress", "tooltip", "typography", "popover", "input", "empty"]
 
 for (let i = 0; i < components.length; i++) {
   await $`npx lerna run build --scope=@illa-design/${components[i]}`
