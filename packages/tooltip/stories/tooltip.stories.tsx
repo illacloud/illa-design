f6440393527085e8ef3527a2a0767ad425993a2e
smallSohoSolo <b3069741@gmail.com>
2022-01-28 16:37:59 +0800

    fix: turborepo problem

--- packages/tooltip/stories/tooltip.stories.tsx
+++ packages/tooltip/stories/tooltip.stories.tsx
@@ -1,8 +1,6 @@
 import * as React from "react"
 import { Meta, Story } from "@storybook/react"
 import { Tooltip } from "../src"
-import results from "../coverage/coverage-final.json"
-import { withTests } from "@storybook/addon-jest"
 import { Button } from "@illa-design/button"
 import { Space } from "@illa-design/space"
 import { TriggerProps } from "@illa-design/trigger"
@@ -11,14 +9,17 @@ import { TriggerProps } from "@illa-design/trigger"
 export default {
   title: "DATA DISPLAY/Tooltip",
   component: Tooltip,
-  decorators: [withTests({ results })],
 } as Meta
 
 const T: Story<TriggerProps> = (args) => {
-  return <Space>
-    <Tooltip {...args}><Button>Hover Here</Button></Tooltip>
-    <Button>Second</Button>
-  </Space>
+  return (
+    <Space>
+      <Tooltip {...args}>
+        <Button>Hover Here</Button>
+      </Tooltip>
+      <Button>Second</Button>
+    </Space>
+  )
 }
 
 export const Basic = T.bind({})
