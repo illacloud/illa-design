72de8eeb802c94da7b1c793ad231c378f271cd15
aoao <zhengjiaqi@illasoft.com>
2022-02-15 17:48:24 +0800

    feat: init upload

---
+++ packages/upload/src/upload.tsx
@@ -0,0 +1,10 @@
+import {forwardRef} from "react";
+import {UploadProps} from "./interface";
+
+export const  Upload = forwardRef<HTMLDivElement, UploadProps>((props,ref)=>{
+
+
+
+
+    return <div> {`Upload`}</div>
+})
---
+++ packages/upload/tests/upload.test.tsx
@@ -0,0 +1,7 @@
+import {render, screen} from "@testing-library/react";
+import {Pagination} from "@illa-design/pagination/src";
+import {globalColor, illaPrefix} from "@illa-design/theme";
+
+test("Upload render correctly", () => {
+
+})
