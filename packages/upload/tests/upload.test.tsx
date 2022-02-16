38e8f14309c87306a69bd36bd5ff0c147c9ced33
aoao <zhengjiaqi@illasoft.com>
2022-02-16 19:26:53 +0800

    Revert "feat: init upload"
    This reverts commit f0cf593a3177484c1b2eab0b83c8f7f080139465.

--- packages/upload/tests/upload.test.tsx
+++
@@ -1,7 +0,0 @@
-import {render, screen} from "@testing-library/react";
-import {Pagination} from "@illa-design/pagination/src";
-import {globalColor, illaPrefix} from "@illa-design/theme";
-
-test("Upload render correctly", () => {
-
-})
