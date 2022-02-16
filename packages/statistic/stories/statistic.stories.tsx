781c185a4e44c69cab80d49aed02e972f13a284b
smallSohoSolo <chenlongbo@illasoft.com>
2022-02-16 15:22:06 +0800

    perf: apply lint

--- packages/statistic/stories/statistic.stories.tsx
+++ packages/statistic/stories/statistic.stories.tsx
@@ -1,8 +1,8 @@
-import * as React from "react";
-import { Meta, Story } from "@storybook/react";
-import { Statistic, StatisticProps } from "../src";
-import { Space } from "@illa-design/space";
-import { ImageDefaultIcon } from "@illa-design/icon";
+import * as React from "react"
+import { Meta, Story } from "@storybook/react"
+import { Statistic, StatisticProps } from "../src"
+import { Space } from "@illa-design/space"
+import { ImageDefaultIcon } from "@illa-design/icon"
 
 export default {
   title: "DATA DISPLAY/Statistic",
