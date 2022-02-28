0fd6066c744f6113a218d94e6d99618c93a80ff4
smallSohoSolo <b3069741@gmail.com>
2022-02-28 19:28:20 +0800

    test: add grid test case

--- packages/grid/tests/row.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from "@testing-library/react"
-import "@testing-library/jest-dom"
-import * as React from "react"
-import { Row } from "../src"
-
-test("Row renders header and footer.", () => {
-  render(<Row data-testid="test-row" />)
-  expect(screen.getByTestId("test-row")).toBeInTheDocument()
-})
