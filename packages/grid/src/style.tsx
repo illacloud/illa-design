17751b098b55f314f16718b8638ed567273b9a85
smallSohoSolo <b3069741@gmail.com>
2022-03-03 19:04:26 +0800

    fix: grid push & pull style

--- packages/grid/src/style.tsx
+++ packages/grid/src/style.tsx
@@ -166,7 +166,7 @@ export function applyColPushStyle(
 ): SerializedStyles {
   if (push) {
     return css`
-      padding-left: calc(${push} * ${oneUnitWidth});
+      left: calc(${push} * ${oneUnitWidth});
     `
   } else {
     return css``
@@ -179,7 +179,7 @@ export function applyColPullStyle(
 ): SerializedStyles {
   if (pull) {
     return css`
-      padding-right: calc(${pull} * ${oneUnitWidth});
+      right: calc(${pull} * ${oneUnitWidth});
     `
   } else {
     return css``
