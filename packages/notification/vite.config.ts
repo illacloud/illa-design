532586faf61569015288bb0df7b0df784abedab2
Jarvey <haojielin.linger@gmail.com>
2022-03-04 16:27:39 +0800

    feat: update vite config

--- packages/notification/vite.config.ts
+++ packages/notification/vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
     sourcemap: true,
     lib: {
       entry: path.resolve(__dirname, "src/index.ts"),
-      name: "@illa-design/pagination",
+      name: "@illa-design/notification",
       fileName: (format) => `${format}/index.js`,
     },
     rollupOptions: {
