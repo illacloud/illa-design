beaad5a38b0ca0e5004c1948570a6a688e6f9e43
smallSohoSolo <b3069741@gmail.com>
2022-03-08 00:38:02 +0800

    fix: change cypress

--- packages/trigger/tests/trigger.e2e.tsx
+++ packages/trigger/tests/trigger.e2e.tsx
@@ -16,18 +16,18 @@ it("Trigger renders correctly", () => {
   unmount()
 })
 
-// it("Trigger renders with close button", () => {
-//   mount(
-//     <Trigger content="Trigger Success Custom" hasCloseIcon trigger="click">
-//       <Button>Hello Trigger Custom</Button>
-//     </Trigger>,
-//   )
-//   cy.findByText("Hello Trigger Custom").trigger("click")
-//   cy.findByText("Close").should("exist")
-//   cy.findByText("Close").trigger("click")
-//   cy.findByText("Trigger Success Custom").should("not.exist")
-//   unmount()
-// })
+it("Trigger renders with close button", () => {
+  mount(
+    <Trigger content="Trigger Success Custom" hasCloseIcon trigger="click">
+      <Button>Hello Trigger Custom</Button>
+    </Trigger>,
+  )
+  cy.findByText("Hello Trigger Custom").trigger("click")
+  cy.findByText("Close").should("exist")
+  cy.findByText("Close").trigger("click")
+  cy.findByText("Trigger Success Custom").should("not.exist")
+  unmount()
+})
 
 it("Trigger renders with different color", () => {
   mount(
