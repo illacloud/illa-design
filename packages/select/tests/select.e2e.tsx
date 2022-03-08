c74523e5c60c17224b583eb7c99b6a56442bc454
xiaoyu-a <lavamint@outlook.com>
2022-03-08 19:06:56 +0800

    feat: add e2e

--- packages/select/tests/select.e2e.tsx
+++ packages/select/tests/select.e2e.tsx
@@ -2,6 +2,7 @@ import * as React from "react"
 import { Select } from "../src"
 import { mount, unmount } from "@cypress/react"
 import "@testing-library/cypress"
+import { waitFor } from "@testing-library/react"
 
 it("Select renders correctly", () => {
   mount(<Select placeholder={"test select"} value={"test"} />)
@@ -13,6 +14,7 @@ it("Select renders correctly", () => {
   mount(<Select error value={"test"} />)
   cy.findByText("test").click()
   expect(cy.findByText("No data")).exist
+  unmount()
 })
 
 it("Select renders with text", () => {
@@ -28,5 +30,23 @@ it("Select renders with text", () => {
   cy.findByText("1").click()
   expect(cy.findByText("1")).exist
   // expect(change).to.be.called
+  unmount()
+})
+
+it("Select renders with multiple", async () => {
+  const change = cy.spy()
+  mount(
+    <Select
+      placeholder={"test"}
+      options={[1, 2, 3]}
+      onVisibleChange={change}
+      mode="multiple"
+    />,
+  )
+  cy.findByPlaceholderText("test").click()
+  cy.findByText("1").click()
+  cy.findByText("2").click()
+  expect(cy.findAllByText("1")).exist
+  expect(cy.findAllByText("2")).exist
   unmount()
 })
