6441839457ed18d4d5219fab861ccb40d78fe079
xiaoyu-a <lavamint@outlook.com>
2022-03-09 20:28:00 +0800

    fix: test

--- packages/select/tests/select.test.tsx
+++ packages/select/tests/select.test.tsx
@@ -1,6 +1,5 @@
 import { Select, Option } from "@illa-design/select"
-import { render, screen, fireEvent, waitFor } from "@testing-library/react"
-import userEvent from "@testing-library/user-event"
+import { render, screen } from "@testing-library/react"
 import "@testing-library/jest-dom"
 
 test("Select renders with text", () => {
