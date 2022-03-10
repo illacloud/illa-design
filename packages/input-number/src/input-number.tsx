c23dcf7937255c79c134dcd359dd28ea5e48943b
xiaoyu-a <lavamint@outlook.com>
2022-03-10 15:34:01 +0800

    feat: add icon

--- packages/input-number/src/input-number.tsx
+++ packages/input-number/src/input-number.tsx
@@ -1,17 +1,23 @@
 /** @jsxImportSource @emotion/react */
 import * as React from "react"
-import { forwardRef } from "react"
+import { forwardRef, useState } from "react"
 import { InputNumberProps } from "./interface"
+import { Input, InputRefType } from "@illa-design/input"
+import { PreIcon, NextIcon } from "@illa-design/icon"
 
-export const InputNumber = forwardRef<HTMLDivElement, InputNumberProps>(
+type StepMethods = 'minus' | 'plus';
+
+export const InputNumber = forwardRef<InputRefType, InputNumberProps>(
   (props, ref) => {
     const {
       style,
       className,
       defaultValue,
       value,
-      disabled,
       error,
+      disabled,
+      readOnly,
+      hideControl,
       placeholder,
       suffix,
       prefix,
@@ -31,10 +37,58 @@ export const InputNumber = forwardRef<HTMLDivElement, InputNumberProps>(
       ...rest
     } = props
 
+    const renderStepButton = !hideControl && !readOnly && mode === "embed"
+    const [inputValue, setInputValue] = useState<string>('');
+    const [isOutOfRange, setIsOutOfRange] = useState(false);
+    const [isUserInputting, setIsUserInputting] = useState(false);
+
+    const handleArrowKey = (event: any, method: StepMethods, needRepeat = false) => {
+      event.persist();
+      event.preventDefault();
+      setIsUserInputting(false);
+
+      if (disabled) {
+        return;
+      }
+
+      let finalValue = min === -Infinity ? 0 : min;
+    };
+
+    const stepButtonEvents = (method: StepMethods) => {
+      return readOnly
+        ? {}
+        : {
+            onMouseDown: (e: any) => handleArrowKey(e, method, true),
+            onMouseLeave: stop,
+            onMouseUp: stop,
+          }
+    }
+
     return (
-      <div style={style} className={className}>
-        inputNumber
-      </div>
+      <Input
+        style={style}
+        className={className}
+        error={error}
+        disabled={disabled}
+        placeholder={placeholder}
+        prefix={prefix}
+        suffix={
+          <>
+            {renderStepButton ? (
+              <div>
+                <div {...stepButtonEvents("plus")}>
+                  {icons && icons.up ? icons.up : <PreIcon />}
+                </div>
+                <div {...stepButtonEvents("minus")}>
+                  {icons && icons.down ? icons.down : <NextIcon />}
+                </div>
+              </div>
+            ) : null}
+            {suffix && <span>{suffix}</span>}
+          </>
+        }
+        {...rest}
+      />
     )
   },
 )
--- packages/input-number/src/interface.tsx
+++ packages/input-number/src/interface.tsx
@@ -14,6 +14,7 @@ export interface InputNumberProps<T = any>
   max?: number
   error?: boolean
   disabled?: boolean
+  readOnly?: boolean
   defaultValue?: number
   value?: number | string
   placeholder?: string
