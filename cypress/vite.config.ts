b410c8ae37e07706b2aaa7be147fb3f52525875e
smallSohoSolo <b3069741@gmail.com>
2022-03-11 15:22:32 +0800

    test: change cypress env

--- cypress/vite.config.ts
+++ cypress/vite.config.ts
@@ -8,9 +8,7 @@ export default defineConfig({
       babel: {
         compact: false,
       },
-      // Exclude storybook stories
       exclude: [/\.stories\.([tj])sx?$/, /\.test\.([tj])sx?$/],
-      // Only .tsx files
       include: ["**/*.tsx", "**/*.ts", "**/*.e2e.ts"],
     }),
   ],
