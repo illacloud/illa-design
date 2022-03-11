5cf61d91bc56899d8c8f723b7ae3429fe394565c
smallSohoSolo <b3069741@gmail.com>
2022-03-11 13:28:38 +0800

    ci: remove unused import

--- cypress/vite.config.ts
+++ cypress/vite.config.ts
@@ -1,8 +1,6 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 
-const path = require("path")
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
