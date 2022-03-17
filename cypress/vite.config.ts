72b0770d292c999b6ec58c7c67dc95ec60acefe7
Spike <l-yanlei@hotmail.com>
2022-03-17 21:28:31 +0800

    chore!(cypress): change `jsxRuntime` to `automatic`
    - https://emotion.sh/docs/css-prop#jsx-pragma

--- cypress/vite.config.ts
+++ cypress/vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react"
 export default defineConfig({
   plugins: [
     react({
-      jsxRuntime: "classic",
+      jsxRuntime: "automatic",
       babel: {
         compact: false,
       },
