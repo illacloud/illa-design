4fb43dcc133f5f2670d994dff21f14d886dbd5dc
xiaoyu-a <lavamint@outlook.com>
2022-03-18 11:02:29 +0800

    feat: back

--- cypress/vite.config.ts
+++ cypress/vite.config.ts
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react"
 export default defineConfig({
   plugins: [
     react({
-      jsxRuntime: "automatic",
+      jsxRuntime: "classic",
       babel: {
         compact: false,
       },
