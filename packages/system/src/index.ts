fffe50372fb4f3af169ec6c56d0967b2de8721e4
Spike <l-yanlei@hotmail.com>
2022-03-21 18:58:48 +0800

    feat: add `easing`

--- packages/system/src/index.ts
+++ packages/system/src/index.ts
@@ -1,4 +1,7 @@
 export * from "./omit"
 export * from "./useMergeValue"
 export * from "./is"
+export * from "./raf"
 export * from "./throttleByRaf"
+import * as easingMethod from "./easing"
+export { easingMethod }
