be8517489f07ec453734a3e3c0bf9a285b879fa1
smallSohoSolo <b3069741@gmail.com>
2022-03-22 12:07:16 +0800

    fix: vite config

--- packages/card/vite.config.ts
+++ packages/card/vite.config.ts
@@ -6,13 +6,22 @@ const path = require("path")
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  assetsInclude: "src/assets/*",
   plugins: [
     react({
+      jsxImportSource: "@emotion/react",
+      jsxRuntime: "automatic",
+      babel: {
+        plugins: ["@emotion/babel-plugin"],
+        compact: false,
+      },
       // Exclude storybook stories
-      exclude: /\.stories\.([tj])sx?$/,
+      exclude: [
+        /\.stories\.([tj])sx?$/,
+        /\.e2e\.([tj])sx?$/,
+        /\.test\.([tj])sx?$/,
+      ],
       // Only .tsx files
-      include: "**/*.tsx",
+      include: ["**/*.tsx", "**/*.ts"],
     }),
   ],
   build: {
@@ -37,6 +46,7 @@ export default defineConfig({
       output: {
         globals: {
           react: "React",
+          "react-dom": "ReactDom",
         },
       },
     },
