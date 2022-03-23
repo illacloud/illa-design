7301bbc0ba6f48f97e56390eac9ad515ce577813
xiaoyu-a <lavamint@outlook.com>
2022-03-23 17:40:20 +0800

    feat: add time picker colum

---
+++ packages/time-picker/src/time-column.tsx
@@ -0,0 +1,119 @@
+import React, { useRef, useEffect, useCallback, forwardRef, useImperativeHandle } from "react"
+import debounce from "lodash/debounce"
+
+type ListItem = {
+  label?: string
+  value?: number | string
+  disabled?: boolean
+  selected?: boolean
+}
+
+export interface TimeColumnProps {
+  list?: ListItem[]
+  value?: number | string
+  onHandleSelect?: (value?: number | string, unit?: string) => void
+  unit?: "hour" | "minute" | "second" | "ampm"
+  popupVisible?: boolean
+  scrollSticky?: boolean
+}
+
+export const TimeColumn = forwardRef<HTMLDivElement, TimeColumnProps>(
+  (props, ref) => {
+    const { list, value, onHandleSelect, unit, popupVisible, scrollSticky } =
+      props
+
+    const lis = useRef<Map<number | string, HTMLElement | null>>(new Map())
+    const wrapper = useRef<HTMLDivElement>(null)
+    const ul = useRef<HTMLUListElement>(null)
+    const listItemHeight = useRef<number>(0)
+
+    const prevPopupVisible = useRef<boolean>()
+    useEffect(() => {
+      prevPopupVisible.current = popupVisible
+    })
+
+    const prevScrollTop = useRef<number>(wrapper?.current?.scrollTop as number)
+
+    useEffect(() => {
+      const li = value ? lis.current.get(value) : undefined
+      if (li && popupVisible && prevPopupVisible.current) {
+        // scrollTo(wrapper.current, li.offsetTop, 150)
+        wrapper.current?.scrollTo({top: li.offsetTop})
+        prevScrollTop.current = li.offsetTop
+      }
+    }, [value])
+
+    useEffect(() => {
+      if (popupVisible && popupVisible !== prevPopupVisible.current) {
+        const li = value ? lis.current.get(value) : undefined
+        if (li) {
+          // scrollTo(wrapper.current, li.offsetTop, 0)
+          wrapper.current?.scrollTo({top: li.offsetTop})
+          prevScrollTop.current = li.offsetTop
+        }
+      }
+    }, [popupVisible, prevPopupVisible])
+
+    useEffect(() => {
+      if (list && list.length <= 1) {
+        return
+      }
+      if (ul.current && wrapper.current && list?.length) {
+        listItemHeight.current =
+          (ul.current.clientHeight - wrapper.current.clientHeight) /
+          (list.length - 1)
+      }
+    }, [list?.length])
+
+    const onScrollList = useCallback(
+      debounce(() => {
+        if (!wrapper.current) {
+          return
+        }
+        const mathFunc =
+          wrapper.current.scrollTop - prevScrollTop.current > 0
+            ? Math.ceil
+            : Math.floor
+        const index = mathFunc(
+          wrapper.current.scrollTop / listItemHeight.current,
+        )
+
+        if (index !== value && list?.[index] && !list[index].disabled) {
+          onHandleSelect?.(list[index].value, unit)
+        }
+      }, 100),
+      [onHandleSelect]
+    )
+
+    useImperativeHandle(ref, () => wrapper.current as HTMLDivElement, [])
+
+    return (
+      <div
+        ref={wrapper}
+        onWheel={() => {
+          if (scrollSticky) {
+            onScrollList()
+          }
+        }}
+      >
+        <ul ref={ul}>
+          {list?.map((item) => {
+            return (
+              <li
+                key={item.value}
+                onClick={() =>
+                  !item.disabled && onHandleSelect?.(item.value, unit)
+                }
+                ref={(element) => {
+                  if (item.value) lis.current?.set(item.value, element)
+                }}
+              >
+                <div>{item.label}</div>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+    )
+  },
+)
---
+++ packages/time-picker/stories/test.stories.tsx
@@ -0,0 +1,110 @@
+import * as React from "react"
+import { Meta, Story } from "@storybook/react"
+import { Tabs, TabsProps, TabPane } from "@illa-design/tabs"
+import { useState } from 'react'
+import { Paragraph} from "@illa-design/typography"
+import { Radio, RadioGroup } from "../../radio/src"
+import { ReactNode } from "react"
+import { BsPlus } from "react-icons/bs"
+
+//👇 This default export determines where your story goes in the story list
+export default {
+  title: "DATA DISPLAY/Tabs",
+  component: Tabs,
+  argTypes: {
+    addIcon: {
+      control: false,
+    },
+    deleteIcon: {
+      control: false,
+    },
+    activeKey: {
+      control: false,
+    },
+    animated: {
+      control: {
+        type: "boolean",
+      },
+    },
+  },
+} as Meta
+
+let count = 5;
+
+const style = { text_align: 'center', marginTop: 20 };
+
+
+const initTabs = [...new Array(count)].map((x, i) => ({
+  title: `Tab ${i + 1}`,
+  key: `key${i + 1}`,
+  content: `${i + 1}`,
+}));
+
+const Template: Story<TabsProps> = (props) => {
+  const [tabs, setTabs] = useState(initTabs);
+  const [activeTab, setActiveTab] = useState('key2');
+  const [variant, setType] = useState('line');
+  const [size, setSize] = useState('default');
+  const handleAddTab = () => {
+    const newTab = {
+      title: `New Tab${++count}`,
+      key: `new key${count}`,
+      content: `${count}`,
+    };
+    setTabs([...tabs, newTab]);
+    setActiveTab(newTab.key);
+  };
+
+  const handleDeleteTab = (key) => {
+    const index = tabs.findIndex((x) => x.key === key);
+    const newTabs = tabs.slice(0, index).concat(tabs.slice(index + 1))
+
+    if (key === activeTab && index > -1 && newTabs.length) {
+      setActiveTab(newTabs[index] ? newTabs[index].key : newTabs[index - 1].key)
+    }
+
+    if (index > -1) {
+      setTabs(newTabs);
+    }
+  };
+
+  return (
+    <div style={{ width: 300, height: 200 }}>
+      <span style={{ marginRight: 20 }}>Size:</span>
+      <RadioGroup name='size' value={size} onChange={()=>setSize} style={{ marginBottom: 24 }}>
+        <Radio value='mini'>mini</Radio>
+        <Radio value='small'>small</Radio>
+        <Radio value='default'>default</Radio>
+        <Radio value='large'>large</Radio>
+      </RadioGroup>
+      <br/>
+      <span style={{ marginRight: 20 }}>Type:</span>
+      <RadioGroup name='type' value={variant} onChange={()=>setType} style={{ marginBottom: 40 }}>
+        <Radio value='line'>line</Radio>
+        <Radio value='card'>card</Radio>
+        <Radio value='card-gutter'>card-gutter</Radio>
+        <Radio value='text'>text</Radio>
+        <Radio value='rounded'>rounded</Radio>
+        <Radio value='capsule'>capsule</Radio>
+      </RadioGroup>
+      <Tabs
+        editable
+        variant={variant}
+        size={size}
+        activeKey={activeTab}
+        onAddTab={handleAddTab}
+        onDeleteTab={handleDeleteTab}
+        onChange={setActiveTab}
+      >
+        {tabs.map((x, i) => (
+          <TabPane destroyOnHide  key={x.key} title={x.title}>
+            <Paragraph style={style} >{`Content of Tab Panel ${x.content}`}</Paragraph>
+          </TabPane>
+        ))}
+      </Tabs>
+    </div>
+  );
+
+}
+
+export const Basiccc = Template.bind({})
