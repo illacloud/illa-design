2d7b99abe7e29d4f0387e696be860a8f507e3821
Spike <l-yanlei@hotmail.com>
2022-03-25 19:22:30 +0800

    feat: add Steps stories: navigation, disabled, switch

--- packages/steps/stories/steps.stories.tsx
+++ packages/steps/stories/steps.stories.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react"
 import { Tooltip } from "@illa-design/tooltip"
 import { Meta, Story } from "@storybook/react"
 import { Space } from "@illa-design/space"
+import { Button } from "@illa-design/button"
 import { HeartIcon, LikeIcon, ShareIcon } from "@illa-design/icon"
 import { Step, Steps, StepsProps } from "../src"
 
@@ -11,8 +12,9 @@ export default {
 } as Meta
 
 const Template: Story<StepsProps> = (args) => {
+  const { onChange, ...restArgs } = args
   return (
-    <Steps {...args}>
+    <Steps {...restArgs}>
       <Step title="Succeeded" />
       <Step title="Processing" />
       <Step title="Pending" />
@@ -107,13 +109,36 @@ export const CustomDot = () => {
   )
 }
 
+export const SwitchStep = () => {
+  const [current, setCurrent] = useState(1)
+
+  return (
+    <Space direction="vertical">
+      <Steps current={current}>
+        <Step title="Succeeded" description="Read the message" />
+        <Step title="Processing" description="Please Confirm" />
+        <Step title="Pending" description="Commit it" />
+      </Steps>
+
+      <Space>
+        <Button disabled={current == 1} onClick={() => setCurrent(current - 1)}>
+          Prev
+        </Button>
+        <Button disabled={current == 3} onClick={() => setCurrent(current + 1)}>
+          Next
+        </Button>
+      </Space>
+    </Space>
+  )
+}
+
 export const OnChange = () => {
   const [current, setCurrent] = useState(1)
   const onChange = (index: number) => {
     setCurrent(index)
   }
   return (
-    <Space direction="vertical">
+    <Space direction="vertical" align="start">
       <Steps onChange={onChange} current={current}>
         <Step title="Succeeded" description="Read the message" />
         <Step title="Processing" description="Please Confirm" />
@@ -133,7 +158,46 @@ export const OnChange = () => {
   )
 }
 
-export const Navigation = Template.bind({})
-Navigation.args = {
-  type: "navigation",
+export const Disabled = () => {
+  const [current, setCurrent] = useState(1)
+  const onChange = (index: number) => {
+    setCurrent(index)
+  }
+  return (
+    <Steps onChange={onChange} current={current}>
+      <Step title="Succeeded" description="Read the message" />
+      <Step title="Processing" description="Please Confirm" />
+      <Step title="Pending" description="Commit it" disabled />
+    </Steps>
+  )
+}
+
+export const Navigation = () => {
+  const [current, setCurrent] = useState(1)
+  const onChange = (index: number) => {
+    setCurrent(index)
+  }
+  return (
+    <Space direction="vertical">
+      <Steps
+        type="navigation"
+        current={current}
+        style={{ width: 800, marginBottom: 20 }}
+      >
+        <Step title="Succeeded" />
+        <Step title="Processing" />
+        <Step title="Pending" />
+      </Steps>
+      <Steps
+        type="navigation"
+        onChange={onChange}
+        current={current}
+        style={{ width: 800 }}
+      >
+        <Step title="Succeeded" description="Read the message" />
+        <Step title="Processing" description="Please Confirm" />
+        <Step title="Pending" description="Commit it" />
+      </Steps>
+    </Space>
+  )
 }
