a48b592355b94b8e953737599d94581a3fe3d419
Spike <l-yanlei@hotmail.com>
2022-03-29 17:00:47 +0800

    feat: stub skeleton work

--- packages/skeleton/src/skeleton.tsx
+++ packages/skeleton/src/skeleton.tsx
@@ -1,11 +1,72 @@
 import { forwardRef } from "react"
-import { SkeletonProps } from "./interface"
+import { isObject } from "@illa-design/system"
+import {
+  SkeletonProps,
+  SkeletonImageProps,
+  SkeletonTextProps,
+} from "./interface"
+import { Text } from "./text"
+import { Image } from "./image"
+import {
+  applyAnimation,
+  applySkeletonStyle,
+  skeletonImageStyle,
+  skeletonTextStyle,
+} from "./style"
 
-export const Skeleton = forwardRef<HTMLDivElement, SkeletonProps>((props, ref) => {
+function getProps(props: boolean | Object) {
+  return isObject(props) ? props : {}
+}
 
-  return (
-    <div ref={ref}></div>
-  )
-})
+export const Skeleton = forwardRef<HTMLDivElement, SkeletonProps>(
+  (props, ref) => {
+    const {
+      style,
+      className,
+      animation = false,
+      visible = true,
+      image,
+      text = true,
+      children,
+      ...restProps
+    } = props
+
+    function renderText() {
+      if (!text) {
+        return null
+      }
+
+      const textProps = getProps(text) as SkeletonTextProps
+
+      return <Text css={skeletonTextStyle} {...textProps} animation={animation} />
+    }
+
+    function renderImage() {
+      if (!image) {
+        return null
+      }
+      const imageProps = getProps(image) as SkeletonImageProps
+
+      return <Image css={skeletonImageStyle} {...imageProps} animation={animation} />
+    }
+
+    function renderSkeleton() {
+      return (
+        <div
+          ref={ref}
+          css={applySkeletonStyle(animation)}
+          className={className}
+          style={style}
+          {...restProps}
+        >
+          {renderImage()}
+          {renderText()}
+        </div>
+      )
+    }
+
+    return visible ? renderSkeleton() : children
+  },
+)
 
 Skeleton.displayName = "Skeleton"
--- packages/skeleton/src/style.tsx
+++ packages/skeleton/src/style.tsx
@@ -1,6 +1,105 @@
-import { css } from "@emotion/react"
+import { css, SerializedStyles, keyframes } from "@emotion/react"
 import { globalColor, illaPrefix } from "@illa-design/theme"
+import { SkeletonImageProps, ImageShape, ImageSize } from "./interface"
 
-export function applySkeleton(){
+const backgroundColor = globalColor(`--${illaPrefix}-gray-08`)
 
+export function applySkeletonStyle(animation: boolean): SerializedStyles {
+  return css([
+    css`
+      display: flex;
+    `,
+  ])
+}
+
+export const skeletonTextStyle = css`
+  flex-grow: 1;
+  overflow: hidden;
+`
+export const skeletonImageStyle = css`
+  margin-right: 16px;
+`
+
+export function applyAnimation(animatin: boolean): SerializedStyles {
+  if (!animatin) {
+    return css``
+  }
+
+  const loading = keyframes`
+  0% {
+    background-position: 100% 50%
+  }
+
+  100% {
+    background-position: 0 50%
+  }
+  `
+  const gray = globalColor(`--${illaPrefix}-gray-08`)
+  const darkGray = globalColor(`--${illaPrefix}-gray-06`)
+
+  return css({
+    background: `linear-gradient(90deg, ${gray} 20%, ${darkGray} 35%, ${gray} 65%)`,
+    backgroundSize: "400% 100%",
+    animation: `${loading} 1.5s linear infinite`,
+  })
+}
+
+export function applyLineStyle(width?: number | string): SerializedStyles {
+  return css([
+    css`
+      &:not(last-child) {
+        margin-bottom: 16px;
+      }
+    `,
+    {
+      width,
+      height: 16,
+      backgroundColor,
+    },
+  ])
+}
+
+export const textContainerStyle = css({
+  listStyle: "none",
+  padding: 0,
+  margin: 0,
+})
+
+export function applyImageStyle({
+  shape = "circle",
+  size = "medium",
+}: SkeletonImageProps): SerializedStyles {
+  return css([
+    css({
+      backgroundColor,
+    }),
+    applyImageSize(size),
+    applyImageShape(shape),
+  ])
+}
+
+function applyImageSize(size: ImageSize): SerializedStyles {
+  let width
+
+  if (typeof size === "number") {
+    width = size
+  } else {
+    const sizeMap = {
+      small: 36,
+      medium: 48,
+      large: 60,
+    }
+
+    width = sizeMap[size]
+  }
+
+  return css({ width, height: width })
+}
+
+function applyImageShape(shape: ImageShape): SerializedStyles {
+  return shape === "circle"
+    ? css`
+        border-radius: 50%;
+      `
+    : css``
 }
