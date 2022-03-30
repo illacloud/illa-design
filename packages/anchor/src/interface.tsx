4649038b5080f66b5680c4e055031474e934b208
Spike <l-yanlei@hotmail.com>
2022-03-30 14:49:00 +0800

    feat: add basic link component

--- packages/anchor/src/interface.tsx
+++ packages/anchor/src/interface.tsx
@@ -1,4 +1,21 @@
-import { HTMLAttributes, MouseEvent, ReactNode } from "react"
+import { HTMLAttributes, ReactNode, CSSProperties } from "react"
 
-export interface AnchorProps {
+export interface AnchorProps extends Omit<HTMLAttributes<HTMLDivElement>, "onChange" | "onSelect"> {
+  animation?: boolean
+  scrollContainer?: string | HTMLElement | Window
+  boundary?: number | "start" | "center" | "end" | "nearest"
+  hash?: boolean
+  affix?: boolean
+  affixStyle?: CSSProperties
+  offsetTop?: number,
+  offsetBottom?: number
+  onChange?: (newLink: string, oldLink: string) => void
+  onSelect?: (newLink: string, oldLink: string) => void
+  lineless?: boolean
+  /* targetOffset: number */
+}
+
+export interface AnchorLinkProps extends Omit<HTMLAttributes<HTMLAnchorElement>, "title"> {
+  href?: string,
+  title?: string | ReactNode
 }
