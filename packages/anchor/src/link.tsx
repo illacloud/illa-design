cea6e532750d2df6aaaa5492ef164eb4653bfc9b
Spike <l-yanlei@hotmail.com>
2022-03-31 17:25:08 +0800

    feat: render nested link in AnchorLink

--- packages/anchor/src/link.tsx
+++ packages/anchor/src/link.tsx
@@ -1,18 +1,49 @@
+import React, { forwardRef, useImperativeHandle, useRef } from "react"
 import { AnchorLinkProps } from "./interface"
 import { applyLinkStyle } from "./style"
+import { isString } from "@illa-design/system"
+import { ReactElement } from "react"
 
-export function Link(props: AnchorLinkProps) {
-  const { style, className, href = "#", title, ...restProps } = props
+export const Link = forwardRef<HTMLDivElement, AnchorLinkProps>(
+  (props, ref) => {
+    const {
+      style,
+      className,
+      href = "#",
+      title,
+      children,
+      ...restProps
+    } = props
 
-  return (
-    <a
-      css={applyLinkStyle()}
-      href={href}
-      style={style}
-      className={className}
-      {...restProps}
-    >
-      {title}
-    </a>
-  )
-}
+    function renderNestedAnchorLink() {
+      return React.Children.toArray(children).filter(
+        (child) =>
+          child &&
+          (child as ReactElement).type &&
+          ((child as ReactElement).type as { displayName?: string })
+            .displayName === "AnchorLink",
+      )
+    }
+
+    const linkRef = useRef<HTMLDivElement>(null)
+    useImperativeHandle(ref, () => linkRef?.current as HTMLDivElement, [])
+
+    return (
+      <div ref={linkRef}>
+        <a
+          css={applyLinkStyle()}
+          href={href}
+          title={isString(title) ? title : ""}
+          style={style}
+          className={className}
+          {...restProps}
+        >
+          {title}
+        </a>
+        {renderNestedAnchorLink()}
+      </div>
+    )
+  },
+)
+
+Link.displayName = "AnchorLink"
