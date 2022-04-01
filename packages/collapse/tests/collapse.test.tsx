b439fc30b23764cc98b45a4784ac3b2a31a104e2
Jarvey <haojielin.linger@gmail.com>
2022-04-01 15:52:33 +0800

    feat: add collapse template

---
+++ packages/collapse/tests/collapse.test.tsx
@@ -0,0 +1,9 @@
+import { Collapse } from "../src"
+import { render, screen } from "@testing-library/react"
+import "@testing-library/jest-dom"
+import { globalColor, illaPrefix } from "@illa-design/theme"
+
+
+test("Collapse renders with ", () => {
+  render(<Collapse />)
+})
