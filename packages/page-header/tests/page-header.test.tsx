ea1b98b0bb980c98f4519da7d1ea95426019ac12
zch <zhengchunhui@illasoft.com>
2022-04-10 23:34:33 +0800

    feat: page-header finish

--- packages/page-header/tests/page-header.test.tsx
+++ packages/page-header/tests/page-header.test.tsx
@@ -1,11 +1,33 @@
 import { PageHeader } from "../src"
 import { render, screen } from "@testing-library/react"
 import "@testing-library/jest-dom"
-import { globalColor, illaPrefix } from "@illa-design/theme"
-
 
 test("PageHeader renders with ", () => {
   render(
-    <PageHeader />
+    <PageHeader
+      title="IllaDesign"
+      subTitle="This is a description"
+      backIcon
+      onBack={() => {}}
+      breadcrumb={{
+        routes: [
+          {
+            path: "/",
+            breadcrumbName: "Home",
+          },
+          {
+            path: "/channel",
+            breadcrumbName: "Channel",
+          },
+        ],
+      }}
+      extra={<div>Cancel</div>}
+    ></PageHeader>,
   )
+  expect(screen.getByText("IllaDesign")).toBeInTheDocument()
+  expect(screen.getByText("This is a description")).toBeInTheDocument()
+  expect(screen.getByTitle("PreIcon")).toBeInTheDocument()
+  expect(screen.getByText("Cancel")).toBeInTheDocument()
+  expect(screen.getByText("Home")).toBeInTheDocument()
+  expect(screen.getByText("Channel")).toBeInTheDocument()
 })
