fab23d28b3fdfe7db7a043970d0fc59a77652d1d
Spike <l-yanlei@hotmail.com>
2022-04-14 19:41:01 +0800

    fix: fix missing horizontal selected style

--- packages/menu/tests/menu.test.tsx
+++ packages/menu/tests/menu.test.tsx
@@ -19,6 +19,20 @@ test("Menu renders with item", () => {
   expect(screen.getByText("Docs")).toBeInTheDocument()
 })
 
+test("Menu renders with horizontal selected", () => {
+  render(
+    <Menu defaultSelectedKeys={["1"]} data-testid={"menu"}>
+      <Item title={"Blog"} key={"1"} disabled />
+      <Item title={"Tutorial"} key={"2"} />
+      <Item title={"Docs"} key={"3"} />
+      <Item title={"Community"} key={"4"} />
+      <Item title={"Github"} key={"5"} />
+    </Menu>,
+  )
+
+  expect(screen.getByTestId("menu")).toMatchSnapshot()
+})
+
 test("Menu renders with item group", () => {
   render(
     <Menu>
@@ -142,7 +156,7 @@ test("onCollapse should be trigger", () => {
     </Menu>,
   )
 
-  ;(container.querySelector(`[data-menu-collapse-icon]`) as HTMLElement).click()
+    ; (container.querySelector(`[data-menu-collapse-icon]`) as HTMLElement).click()
 
   expect(event).toBeCalled()
 })
