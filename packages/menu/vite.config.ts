0e681d21f907002eb2ec7cfd816d1a7ba4212f0e
AruSeito <weichen@illasoft.com>
2022-04-14 17:51:55 +0800

    build: remove sourcemap and minify

--- packages/menu/vite.config.ts
+++ packages/menu/vite.config.ts
@@ -25,6 +25,8 @@ export default defineConfig({
     }),
   ],
   build: {
+    sourcemap: false,
+    minify: false,
     lib: {
       entry: path.resolve(__dirname, "src/index.ts"),
       name: "@illa-design/menu",
