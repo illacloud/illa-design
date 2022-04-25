957c687fab921b5442be21b9a5982c54d892c3f4
Jarvey <haojielin.linger@gmail.com>
2022-04-25 19:31:26 +0800

    fix: usemodal coverage

--- packages/modal/tests/modal.e2e.tsx
+++ packages/modal/tests/modal.e2e.tsx
@@ -1,8 +1,11 @@
 import * as React from "react"
 import { mount, unmount } from "@cypress/react"
 import "@testing-library/cypress"
-import { Modal, ModalProps } from "../src"
+import { Modal, ModalProps, ModalReturnProps } from "../src"
 import { Button } from "@illa-design/button"
+import { AlertType, AlertType as ConfirmType } from "@illa-design/alert"
+
+const confirmTypes = ["info", "success", "warning", "error", "confirm"]
 
 const DemoTest = (props: ModalProps) => {
   const [visible, setVisible] = React.useState(false)
@@ -43,15 +46,17 @@ const DemoTest = (props: ModalProps) => {
   )
 }
 
-const ContextModal = () => {
+const ContextModal = (props: ModalProps & { type?: ConfirmType }) => {
+  const { type = "info" } = props
   const [modal, contextHolder] = Modal.useModal()
   const ConfigContext = React.createContext({})
   return (
     <ConfigContext.Provider value="Jarvey">
       {contextHolder}
       <Button
-        onClick={() =>
-          modal.info({
+        onClick={() => {
+          modal[type]({
+            ...props,
             content: (
               <ConfigContext.Consumer>
                 {(name) => `Current user: ${name}`}
@@ -59,9 +64,36 @@ const ContextModal = () => {
             ),
             title: "Context",
           })
-        }
+        }}
       >
         Get Context
+      </Button>
+      <Button
+        onClick={() => {
+          modal[type]({ title: "Type", content: `${type}` })
+        }}
+      >
+        {type + "button"}
+      </Button>
+      <Button
+        onClick={() => {
+          let instance: ModalReturnProps
+          instance = modal.info({
+            closable: true,
+            closeElement: <button>close</button>,
+            content: (
+              <Button
+                onClick={() => {
+                  instance.close()
+                }}
+              >
+                Off
+              </Button>
+            ),
+          })
+        }}
+      >
+        Open
       </Button>
     </ConfigContext.Provider>
   )
@@ -106,9 +138,53 @@ it("Modal renders with delay ok", () => {
   unmount()
 })
 
-it("Modal renders with useModal", () => {
+it("useModal api renders with close method", () => {
   mount(<ContextModal />)
-  cy.findByText("Get Context").click()
-  cy.findByText("Current user: Jarvey").should("exist")
+  cy.findByRole("button", { name: "Open" }).click()
+  cy.findByRole("button", { name: "close" }).should("exist")
+  cy.findByRole("button", { name: "Off" }).trigger("click")
+  cy.findByRole("button", { name: "close" }).should("not.exist")
+})
+
+it("useModal renders with onOk event", () => {
+  const handlers = {
+    okResolve: () => Promise.resolve(),
+    okReject: () => Promise.reject(),
+    okNull: () => {},
+  }
+  mount(<ContextModal onOk={cy.spy(handlers, "okResolve").as("okResolve")} />)
+  cy.findByRole("button", { name: "Get Context" }).click()
+  cy.findByRole("button", { name: "OK" }).click().should("not.exist")
+  cy.get("@okResolve").should("to.be.called")
   unmount()
+  mount(<ContextModal onOk={cy.spy(handlers, "okReject").as("okReject")} />)
+  cy.findByRole("button", { name: "Get Context" }).click()
+  cy.findByRole("button", { name: "OK" }).click()
+  cy.get("@okReject").should("to.be.called")
+  unmount()
+  mount(<ContextModal onOk={cy.spy(handlers, "okNull").as("okNull")} />)
+  cy.findByRole("button", { name: "Get Context" }).click()
+  cy.findByRole("button", { name: "OK" }).click().should("not.exist")
+  cy.get("@okNull").should("to.be.called")
+  unmount()
+})
+
+it("Modal renders with useModal context and afterCloseEvent", () => {
+  const afterCloseEvent = cy.spy().as("afterCloseEvent")
+  mount(<ContextModal afterClose={afterCloseEvent} />)
+  cy.findByRole("button", { name: "Get Context" }).click()
+  cy.findByText(`Current user: Jarvey`).should("exist")
+  cy.root().click()
+  cy.get("@afterCloseEvent").should("to.be.called")
+  unmount()
+})
+
+it("Modal renders with useModal different types", () => {
+  confirmTypes.forEach((type) => {
+    mount(<ContextModal type={type as AlertType} />)
+    cy.findByRole("button", { name: `${type}button` }).click()
+    cy.findByText(`${type}`).should("exist")
+    cy.root().click()
+    unmount()
+  })
 })
