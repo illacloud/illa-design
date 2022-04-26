4c735058d0aa352888dd9f6a5567a413bdda3b4b
Spike <l-yanlei@hotmail.com>
2022-04-27 01:28:29 +0800

    refactor: add branch coverage in report script

--- scripts/merge-coverage.sh
+++ scripts/merge-coverage.sh
@@ -3,4 +3,4 @@ mkdir reports || true
 cp cypress-coverage/lcov.info reports/from-cypress.info && cp jest-coverage/lcov.info reports/from-jest.info
 # mkdir coverage || true
 # ./scripts/mergeLcov.perl -a ./reports/from-cypress.info -a ./reports/from-jest.info -o ./coverage/lcov.info
-# ./scripts/genHtml.perl ./coverage/lcov.info --output-directory=./coverage/lcov-report
+./scripts/genHtml.perl --legend --branch-coverage -t merged-coverage-overview ./reports/from-cypress.info ./reports/from-jest.info --o=./coverage/lcov-report
