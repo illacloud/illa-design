f56e4152d5af84bf8c8f00cda9763fd4c4c6a070
Spike <l-yanlei@hotmail.com>
2022-04-28 01:28:50 +0800

    feat: add component coverage scripts

---
+++ scripts/coverage-component.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+name=""
+
+if test $1; then name=$1;
+else
+read -p "Component Name: " component;
+name=component
+fi
+
+echo "Generate $name ‘s coverage report..."
+
+yarn jest packages/$name
+
+CYFILE=packages/$name/tests/$name.e2e.tsx
+
+if test -f "$CYFILE"
+then
+"yarn cypress run-ct -b chrome -s $CYFILE";
+fi
+
+yarn report:combined
+
+if command -v open
+then open ./coverage/lcov-report/index.html
+fi
