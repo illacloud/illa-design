285b4f0c298a99e7be6b4295113d3fd7025508cb
zch <zhengchunhui@illasoft.com>
2022-05-12 15:10:12 +0800

    fix: remove annotation

--- packages/calendar/src/styles.tsx
+++ packages/calendar/src/styles.tsx
@@ -3,7 +3,6 @@ import { globalColor, illaPrefix } from "@illa-design/theme"
 
 export const bodyContentCss = css`
   position: relative;
-  // padding: 0 10px;
   overflow: auto;
   ::-webkit-scrollbar {
     display: none;
