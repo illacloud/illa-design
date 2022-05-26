fd106af14208cc7a160b09476acd1bb7d147c5b5
smallSohoSolo <b3069741@gmail.com>
2022-05-26 14:50:19 +0800

    feat: change sb to vite

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const react = require("@vitejs/plugin-react")
+
 module.exports = {
   stories: ["../packages/**/stories/*.stories.tsx"],
   addons: [
@@ -11,12 +13,24 @@ module.exports = {
     fastRefresh: true,
     strictMode: true,
   },
-  webpackFinal: async (config, { configType }) => {
-    config.module.rules.push({
-      type: "javascript/auto",
-      test: /\.mjs$/,
-      include: /node_modules/,
-    })
+  core: { builder: "@storybook/builder-vite" },
+  viteFinal: async (config, { configType }) => {
+    config.plugins = config.plugins.filter(
+      (plugin) =>
+        !(Array.isArray(plugin) && plugin[0]?.name.includes("vite:react")),
+    )
+    config.plugins.push(
+      react({
+        jsxImportSource: "@emotion/react",
+        jsxRuntime: "automatic",
+        babel: {
+          plugins: ["@emotion/babel-plugin"],
+          compact: false,
+        },
+        exclude: [/\.stories\.([tj])sx?$/, /\.test\.([tj])sx?$/],
+        include: ["**/**.tsx", "**/**.ts"],
+      }),
+    )
     return config
   },
 }
