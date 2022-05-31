02e6c5e4eec70583a1040a13a708deb9360e35c1
smallSohoSolo <b3069741@gmail.com>
2022-05-31 16:08:12 +0800

    feat: add padding vertical to empty & update sb

--- packages/empty/src/interface.tsx
+++ packages/empty/src/interface.tsx
@@ -2,6 +2,7 @@ import { ReactNode, HTMLAttributes } from "react"
 
 export interface EmptyProps extends HTMLAttributes<HTMLDivElement> {
   description?: ReactNode
+  paddingVertical?: string
   icon?: ReactNode
   imgSrc?: string
 }
