881a7c10c2e5595e9fc767106f21969ef70ecee4
Spike <l-yanlei@hotmail.com>
2022-06-07 16:12:57 +0800

    style: remove empty line

--- packages/icon/src/index.ts
+++ packages/icon/src/index.ts
@@ -83,6 +83,5 @@ export * from "./icons/string"
 export * from "./icons/postgres"
 export * from "./icons/my-sql"
 export * from "./icons/redis"
-
 export * from "./icons/mongo-db"
 export * from "./icons/java-script-transfomer"
