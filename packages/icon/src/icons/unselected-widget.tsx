64f7203b2500f2e0c5b8a07f65850882069e9c32
AruSeito <weichen@illasoft.com>
2022-06-09 18:38:04 +0800

    feat: add unselectedIcon

---
+++ packages/icon/src/icons/unselected-widget.tsx
@@ -0,0 +1,30 @@
+import { createIcon } from "../create-icon"
+
+export const UnselectedWidgetIcon = createIcon({
+  title: "unselectedWidgetIcon",
+  viewBox: "0 0 31 33",
+  path: (
+    <>
+      <path
+        d="M17 25H3C1.89543 25 1 24.1046 1 23V3C1 1.89543 1.89543 1 3 1H11H15H23C24.1046 1 25 1.89543 25 3V17"
+        stroke="#A9AEB8"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+      />
+      <path
+        d="M22 3H14.6667H11.3333H4C3.44771 3 3 3.44771 3 4V22C3 22.5523 3.44771 23 4 23H16.3333H22C22.5523 23 23 22.5523 23 22V16.3333V4C23 3.44771 22.5523 3 22 3Z"
+        fill="#F2F3F5"
+      />
+      <path
+        d="M24.9759 26.3987L23.0592 31.3089L19.9961 20.464L30.1443 25.3638L24.9759 26.3987Z"
+        fill="white"
+        stroke="#A9AEB8"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </>
+  ),
+})
+
+UnselectedWidgetIcon.displayName = "unselectedWidgetIcon"
