c36f2a5ef253c96b646cd2aa7e4a860e181c218a
smallSohoSolo <b3069741@gmail.com>
2022-06-20 19:08:05 +0800

    feat: add drag icon

---
+++ packages/icon/src/icons/drag.tsx
@@ -0,0 +1,16 @@
+import { createIcon } from "../create-icon"
+
+export const DragIcon = createIcon({
+  title: "DragIcon",
+  viewBox: "0 0 12 12",
+  path: (
+    <path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M8 8.75a.25.25 0 0 1 .25.25v1a.25.25 0 0 1-.25.25H7a.25.25 0 0 1-.25-.25V9A.25.25 0 0 1 7 8.75h1zm-3 0a.25.25 0 0 1 .25.25v1a.25.25 0 0 1-.25.25H4a.25.25 0 0 1-.25-.25V9A.25.25 0 0 1 4 8.75h1zm2-3.5h1a.25.25 0 0 1 .25.25v1a.25.25 0 0 1-.25.25H7a.25.25 0 0 1-.25-.25v-1A.25.25 0 0 1 7 5.25zm-3 0h1a.25.25 0 0 1 .25.25v1a.25.25 0 0 1-.25.25H4a.25.25 0 0 1-.25-.25v-1A.25.25 0 0 1 4 5.25zm4-3.5a.25.25 0 0 1 .25.25v1a.25.25 0 0 1-.25.25H7A.25.25 0 0 1 6.75 3V2A.25.25 0 0 1 7 1.75h1zm-3 0a.25.25 0 0 1 .25.25v1a.25.25 0 0 1-.25.25H4A.25.25 0 0 1 3.75 3V2A.25.25 0 0 1 4 1.75h1z"
+      fill="currentColor"
+    />
+  ),
+})
+
+DragIcon.displayName = "DragIcon"
