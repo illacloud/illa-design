e4a19f52a21b4a70f6e3dec200a2d4acfec385fd
xiaoyu-a <lavamint@outlook.com>
2022-07-12 16:16:54 +0800

    feat: update story

--- packages/input/stories/input.stories.tsx
+++ packages/input/stories/input.stories.tsx
@@ -2,9 +2,6 @@ import { Meta, Story } from "@storybook/react"
 import { Input, InputProps } from "../src"
 import { Space } from "@illa-design/space"
 import { PersonIcon } from "@illa-design/icon"
-import { useRef, useState } from "react"
-import { getColor } from "@illa-design/theme"
-import { colorPalette } from "@illa-design/theme/src"
 
 //👇 This default export determines where your story goes in the story list
 export default {
@@ -52,20 +49,7 @@ export default {
 } as Meta
 
 const Template: Story<InputProps> = (props) => {
-  const [color, setColor] = useState("black")
-  const [step, setStep] = useState("01")
 
-  function generate(color: string) {
-    const format = "hex"
-    const list = []
-    const func = colorPalette
-    for (let i = 1; i <= 10; i++) {
-      list.push(func(color, i, format))
-    }
-    return list
-  }
-
-  console.log(generate("red"))
   return (
     <div>
       <Space direction={"vertical"} wrap>
@@ -91,22 +75,6 @@ const Template: Story<InputProps> = (props) => {
           addonAfter={{ render: "After" }}
           {...props}
         />
-        <div style={{ backgroundColor: getColor(color, step), padding: 10 }}>
-          <input
-            type="text"
-            defaultValue={color}
-            onChange={(e) => {
-              setColor(e.target.value)
-            }}
-          />
-          <input
-            type="text"
-            defaultValue={step}
-            onChange={(e) => {
-              setStep(e.target.value)
-            }}
-          />
-        </div>
       </Space>
     </div>
   )
