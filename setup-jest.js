650250180dcafd1c8496baf8249b409ab59dfd8b
xiaoyu <lavamint@outlook.com>
2022-07-15 12:55:44 +0800

    style: lint

--- setup-jest.js
+++ setup-jest.js
@@ -1,11 +1,11 @@
 class IntersectionObserver {
-    observe = jest.fn()
-    disconnect = jest.fn()
-    unobserve = jest.fn()
+  observe = jest.fn()
+  disconnect = jest.fn()
+  unobserve = jest.fn()
 }
 
-Object.defineProperty(global, 'IntersectionObserver', {
-    writable: true,
-    configurable: true,
-    value: IntersectionObserver,
+Object.defineProperty(global, "IntersectionObserver", {
+  writable: true,
+  configurable: true,
+  value: IntersectionObserver,
 })
