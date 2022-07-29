f6ecb8a0aa956fb0dcd80c8410748758c573f249
smallSohoSolo <b3069741@gmail.com>
2022-07-29 19:04:43 +0800

    feat: update pen icon

--- packages/icon/src/icons/pen.tsx
+++ packages/icon/src/icons/pen.tsx
@@ -2,13 +2,13 @@ import { createIcon } from "../create-icon"
 
 export const PenIcon = createIcon({
   title: "PenIcon",
-  viewBox: "0 0 16 16",
+  viewBox: "0 0 14 14",
   path: (
     <path
       fillRule="evenodd"
       clipRule="evenodd"
-      d="m10.623 2 3.188 3.188-8.622 8.623H2v-3.189L10.623 2zM3.5 11.244v1.067h1.068l4.994-4.995-1.067-1.067L3.5 11.244zm6.056-6.056 1.067 1.067 1.067-1.067-1.067-1.067-1.067 1.067z"
-      fill="currentColor"
+      d="m9.295 1.75 2.79 2.79-7.545 7.545H1.75v-2.79L9.295 1.75zM3.063 9.838v.934h.934l4.37-4.37-.934-.934-4.37 4.37zM8.36 4.54l.934.933.933-.933-.933-.934-.934.934z"
+      fill="#BBC0C9"
     />
   ),
 })
