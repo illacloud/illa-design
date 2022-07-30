523df71af05bd31388d7fe70bce53bc5b955b441
smallSohoSolo <b3069741@gmail.com>
2022-07-30 18:53:10 +0800

    chore: change pen icon

--- packages/icon/src/icons/pen.tsx
+++ packages/icon/src/icons/pen.tsx
@@ -8,7 +8,7 @@ export const PenIcon = createIcon({
       fillRule="evenodd"
       clipRule="evenodd"
       d="m9.295 1.75 2.79 2.79-7.545 7.545H1.75v-2.79L9.295 1.75zM3.063 9.838v.934h.934l4.37-4.37-.934-.934-4.37 4.37zM8.36 4.54l.934.933.933-.933-.933-.934-.934.934z"
-      fill="#BBC0C9"
+      fill="currentColor"
     />
   ),
 })
