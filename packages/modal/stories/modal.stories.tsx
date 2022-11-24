cb60bb214620a1d40b6b5b4bd2255cccd8334433
smallSohoSolo <b3069741@gmail.com>
2022-11-24 22:28:44 +0800

    fix: message & notification

--- packages/modal/stories/modal.stories.tsx
+++ packages/modal/stories/modal.stories.tsx
@@ -4,8 +4,8 @@ import { Modal, ModalProps } from "../src"
 import { Space } from "@illa-design/space"
 import { Button } from "@illa-design/button"
 import { Input } from "@illa-design/input"
-import { Message } from "@illa-design/message"
 import { InfoCircleIcon, SuccessIcon } from "@illa-design/icon"
+import { MessageGroup, useMessage } from "@illa-design/message"
 
 export default {
   title: "FEEDBACK/Modal",
@@ -23,11 +23,15 @@ const Template: Story<ModalProps> = (args) => {
     })
   }
 
+  const message = useMessage()
+
   function onOk() {
     Promise.resolve().then((res) => {
       setConfirmLoading(true)
       setTimeout(() => {
-        Message.success("Success !")
+        message.success({
+          content: "Success !",
+        })
         setVisible(false)
         setConfirmLoading(false)
       }, 1500)
@@ -35,60 +39,63 @@ const Template: Story<ModalProps> = (args) => {
   }
 
   return (
-    <Space size={"large"}>
-      <Button
-        onClick={() => {
-          setVisible(true)
-        }}
-      >
-        Open Modal
-      </Button>
-      <Modal
-        {...args}
-        visible={visible}
-        confirmLoading={confirmLoading}
-        onCancel={() => setVisible(false)}
-        onOk={onOk}
-      >
-        ILLA very good!
-      </Modal>
-
-      <Space size={"large"} direction={"vertical"}>
+    <>
+      <MessageGroup />
+      <Space size={"large"}>
         <Button
-          onClick={() =>
-            Modal.confirm({ content: <Input />, title: "Confirm" })
-          }
-        >
-          Confirm
-        </Button>
-        <Button onClick={() => Modal.warning({ title: "Warning" })}>
-          Warning
-        </Button>
-        <Button
-          onClick={async () => {
-            const modalIns = Modal.confirm({
-              title: "Submitting...",
-              icon: <InfoCircleIcon />,
-              content: "This modal will be successful after 1.5s.",
-              footer: false,
-              isNotice: true,
-              noticeType: "info",
-            })
-            await sleep(1500)
-            modalIns.update({
-              icon: <SuccessIcon />,
-              title: "Success",
-              content: "This modal will be closed after 1.5s.",
-              noticeType: "success",
-            })
-            await sleep(1500)
-            modalIns.close()
+          onClick={() => {
+            setVisible(true)
           }}
         >
-          Update & Remove
+          Open Modal
         </Button>
+        <Modal
+          {...args}
+          visible={visible}
+          confirmLoading={confirmLoading}
+          onCancel={() => setVisible(false)}
+          onOk={onOk}
+        >
+          ILLA very good!
+        </Modal>
+
+        <Space size={"large"} direction={"vertical"}>
+          <Button
+            onClick={() =>
+              Modal.confirm({ content: <Input />, title: "Confirm" })
+            }
+          >
+            Confirm
+          </Button>
+          <Button onClick={() => Modal.warning({ title: "Warning" })}>
+            Warning
+          </Button>
+          <Button
+            onClick={async () => {
+              const modalIns = Modal.confirm({
+                title: "Submitting...",
+                icon: <InfoCircleIcon />,
+                content: "This modal will be successful after 1.5s.",
+                footer: false,
+                isNotice: true,
+                noticeType: "info",
+              })
+              await sleep(1500)
+              modalIns.update({
+                icon: <SuccessIcon />,
+                title: "Success",
+                content: "This modal will be closed after 1.5s.",
+                noticeType: "success",
+              })
+              await sleep(1500)
+              modalIns.close()
+            }}
+          >
+            Update & Remove
+          </Button>
+        </Space>
       </Space>
-    </Space>
+    </>
   )
 }
 
--- packages/notification/src/index.ts
+++ packages/notification/src/index.ts
@@ -1,3 +1,4 @@
 export * from "./interface"
 export * from "./notification"
 export * from "./style"
+export * from "./notification-group"
