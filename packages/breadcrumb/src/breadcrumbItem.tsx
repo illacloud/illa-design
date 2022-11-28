652d548b479eaa4ae9ddd9d53bfbd011c1171afb
smallSohoSolo <b3069741@gmail.com>
2022-11-28 21:36:01 +0800

    feat: refactor breadcrumb

--- packages/breadcrumb/src/breadcrumbItem.tsx
+++ packages/breadcrumb/src/breadcrumbItem.tsx
@@ -1,59 +1,28 @@
-import { forwardRef, useCallback, useContext, useMemo } from "react"
+import { forwardRef } from "react"
 import { BreadcrumbItemProps } from "./interface"
-import { itemCss, itemCurrentCss, itemHoverCss } from "./style"
-import { css } from "@emotion/react"
-import { BreadcrumbContext } from "./breadcrumb-context"
-import { Dropdown } from "@illa-design/dropdown"
-import { Menu } from "@illa-design/menu"
+import { applyItemStyle, itemFinalStyle } from "./style"
 import { applyBoxStyle, deleteCssProps } from "@illa-design/theme"
-
-const { Item } = Menu
+import { Dropdown } from "@illa-design/dropdown"
+import { DownIcon } from "@illa-design/icon"
 
 export const BreadcrumbItem = forwardRef<HTMLDivElement, BreadcrumbItemProps>(
   (props, ref) => {
-    const { dropList, dropdownProps, ...restProps } = props
-
-    const { isCurrent, path, breadcrumbName, children } =
-      useContext(BreadcrumbContext)
-
-    const dropItem = useMemo(() => {
-      return (
-        children?.length && (
-          <Menu>
-            {children.map((cItem: any, cIdx: number) => {
-              return <Item key={cIdx + ""} title={cItem.breadcrumbName}></Item>
-            })}
-          </Menu>
-        )
-      )
-    }, [children])
-
-    const finalDrop = dropList || dropItem
-
-    const handleItemCss = useCallback(() => {
-      return css`
-        ${itemCss};
-        ${isCurrent && itemCurrentCss};
-        ${path && itemHoverCss}
-      `
-    }, [isCurrent, path])
-
-    const item = (
-      <div
-        ref={ref}
-        css={css(handleItemCss(), applyBoxStyle(props))}
-        {...deleteCssProps(restProps)}
-      >
-        {breadcrumbName || props.children}
-      </div>
-    )
-
-    return finalDrop ? (
-      <Dropdown dropList={finalDrop} {...dropdownProps}>
-        {item}
+    const { dropList, dropdownProps, children, last, href, ...restProps } =
+      props
+    return (
+      <Dropdown position="bottom-start" dropList={dropList} {...dropdownProps}>
+        <a
+          css={[
+            last ? itemFinalStyle : applyItemStyle(href),
+            applyBoxStyle(props),
+          ]}
+          href={href}
+          {...deleteCssProps(restProps)}
+        >
+          {children}
+          {dropList && <DownIcon ml="2px" />}
+        </a>
       </Dropdown>
-    ) : (
-      item
     )
   },
 )
