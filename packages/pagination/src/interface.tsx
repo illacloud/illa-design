c945e655e1da0011951f7cb7020ff19a44ef834b
smallSohoSolo <b3069741@gmail.com>
2022-12-09 16:42:06 +0800

    chore: update pagination

--- packages/pagination/src/interface.tsx
+++ packages/pagination/src/interface.tsx
@@ -1,72 +1,36 @@
 import { HTMLAttributes, ReactNode } from "react"
 import { BoxProps } from "@illa-design/theme"
+import { SelectProps } from "@illa-design/select"
 
 export type PaginationSize = "small" | "medium" | "large"
+export type PaginationItemType = "page" | "more" | "prev" | "next"
 
 export interface PaginationProps
   extends Omit<HTMLAttributes<HTMLDivElement>, "onChange">,
     BoxProps {
-  currentPage?: number
-  pageSize?: number
-  total?: number
-  defaultCurrent?: number
-  defaultPageSize?: number
   disabled?: boolean
   hideOnSinglePage?: boolean
-  size?: PaginationSize
-  showTotal?: boolean | ((total: number, range: number[]) => ReactNode)
-  sizeCanChange?: boolean
-  sizeOptions?: number[]
-  simple?: boolean
+  pageSizeChangeResetCurrent?: boolean
   showJumper?: boolean
-  placeholder?: string
+  showMore?: boolean
+  simple?: boolean
+  sizeCanChange?: boolean
+  bufferSize?: number
+  current?: number
+  defaultCurrent?: number
+  pageSize?: number
+  defaultPageSize?: number
+  total?: number
+  itemRender?: (
+    page: number,
+    type: PaginationItemType,
+    originElement: ReactNode,
+  ) => ReactNode
+  size?: PaginationSize
+  icons: { prev?: ReactNode; next?: ReactNode; more?: ReactNode }
+  selectProps?: Partial<SelectProps>
+  sizeOptions?: number[]
   onChange?: (pageNumber: number, pageSize: number) => void
   onPageSizeChange?: (size: number, current: number) => void
-  prevIcon?: ReactNode
-  nextIcon?: ReactNode
-  moreIcon?: ReactNode
-}
-
-export interface PageNumProps extends HTMLAttributes<HTMLSpanElement> {
-  index: number | string
-  selected: boolean
-  moreIcon?: ReactNode
-  isMoreIndex: boolean | undefined
-  handleClick: (index: number) => void
-  wholeDisable: boolean | undefined
-  size: PaginationSize
-}
-
-export interface PageNumGroupProps extends HTMLAttributes<HTMLSpanElement> {
-  total: number
-  moreIcon?: ReactNode
-  selectedIndex: number
-  onCurPageIndexChanged: (index: number) => void
-  wholeDisable: boolean | undefined
-  size: PaginationSize
-}
-
-export interface JumperInputProps extends HTMLAttributes<HTMLSpanElement> {
-  onEnterPress: (pageNum: number) => void
-  wholeDisable: boolean | undefined
-  size: PaginationSize
-  totalPages: number
-  currentPage: number
-}
-
-export interface SimplePaginationProps extends HTMLAttributes<HTMLSpanElement> {
-  defaultSelectedPageIndex: number
-  onCurrentChange: (pageNum: number) => void
-  pageSum: number
-  prevIcon?: ReactNode
-  nextIcon?: ReactNode
-  wholeDisabled: boolean
-  size: PaginationSize
-}
-
-export interface PageSizeSelectorProps extends HTMLAttributes<HTMLSpanElement> {
-  onPageSizeSelected: (pageSize: number) => void
-  sizeOptions: number[]
-  wholeDisable: boolean | undefined
-  size: PaginationSize
+  showTotal?: boolean | ((total: number, range: number[]) => ReactNode)
 }
--- packages/pagination/src/pagination.tsx
+++ packages/pagination/src/pagination.tsx
@@ -1,204 +1,74 @@
 import * as React from "react"
-import { forwardRef, ReactNode, useContext } from "react"
-import { MoreIcon, NextIcon, PreIcon } from "@illa-design/icon"
+import { forwardRef, useContext } from "react"
+import { PaginationProps } from "./interface"
+import {
+  jumperContainerStyle,
+  paginationContainer,
+  totalTextStyle,
+} from "./style"
 import {
   ConfigProviderContext,
   ConfigProviderProps,
   def,
 } from "@illa-design/config-provider"
-import { applyBoxStyle, deleteCssProps } from "@illa-design/theme"
-import {
-  applyDefaultItemCss,
-  applyDefaultItemWithMarginCss,
-  paginationContainer,
-  totalTextCss,
-} from "./style"
-import { PageNumGroup } from "./page-num-group"
-import { JumperInput } from "./jump-input"
-import { PageSizeSelector } from "./page-size-selector"
-import { SimplePagination } from "./simple-pagination"
-import { PaginationProps } from "./interface"
 import { useMergeValue } from "@illa-design/system"
+import { Input } from "@illa-design/input"
 
 export const Pagination = forwardRef<HTMLDivElement, PaginationProps>(
   (props, ref) => {
     const {
-      currentPage,
-      pageSize,
-      total = 0,
-      defaultCurrent = 1,
-      defaultPageSize = 10,
       disabled,
-      hideOnSinglePage = true,
-      size = "medium",
-      showTotal,
-      sizeCanChange = true,
-      sizeOptions = [10, 20, 30, 40, 50],
-      simple,
+      hideOnSinglePage,
+      pageSizeChangeResetCurrent,
       showJumper,
-      placeholder,
+      showMore,
+      simple,
+      sizeCanChange,
+      bufferSize,
+      current,
+      defaultCurrent,
+      pageSize,
+      defaultPageSize,
+      total = 0,
+      itemRender,
+      size,
+      icons,
+      selectProps,
+      sizeOptions,
       onChange,
       onPageSizeChange,
-      prevIcon,
-      nextIcon,
-      moreIcon,
-      ...otherProps
+      showTotal,
+      // ...otherProps
     } = props
-
-    const [curPage, setCurPage] = useMergeValue(0, {
-      defaultValue: defaultCurrent - 1,
-      value: currentPage,
-    })
-    const [itemCount, setItemCount] = useMergeValue(10, {
-      defaultValue: defaultPageSize,
-      value: pageSize,
-    })
 
     const configProviderProps = useContext<ConfigProviderProps>(
       ConfigProviderContext,
     )
+
     const locale = configProviderProps?.locale?.pagination ?? def.pagination
 
-    const totalText = locale["total"]
+    const [finalCurrent, setFinalCurrent] = useMergeValue({
+      defaultValue: defaultCurrent,
+      value: current,
+    })
 
-    let pageSum: number
-    if (total <= 0) {
-      pageSum = 0
-    } else {
-      pageSum = Math.ceil(total / itemCount)
-    }
-
-    const prevDisable = curPage == 0 || disabled || pageSum == 0
-    const nextDisable = curPage == pageSum - 1 || disabled || pageSum == 0
-
-    if (hideOnSinglePage && pageSum == 1) {
-      return <></>
-    }
-
-    let totalElement: ReactNode = null
-    if (typeof showTotal === "boolean" && showTotal) {
-      totalElement = (
-        <span css={totalTextCss}>
-          {totalText.replace("{0}", total.toString())}
-        </span>
-      )
-    }
-
-    if (typeof showTotal === "function") {
-      totalElement = (
-        <div css={totalTextCss}>
-          {showTotal(total, [
-            curPage * itemCount + 1,
-            (curPage + 1) * itemCount,
-          ])}
-        </div>
-      )
-    }
-
-    const _preIcon: ReactNode = prevIcon ?? <PreIcon />
-    const _nextIcon: ReactNode = nextIcon ?? <NextIcon />
-    const _moreIcon: ReactNode = moreIcon ?? <MoreIcon />
-
-    const changeCurrentPage = (currentPage: number) => {
-      if (!("currentPage" in props)) {
-        setCurPage(currentPage)
-      }
-    }
-    const changePageSize = (pageSize: number) => {
-      if (!("pageSize" in props)) {
-        setItemCount(pageSize)
-      }
-    }
+    const [finalPageSize, setFinalPageSize] = useMergeValue({
+      defaultValue: defaultPageSize,
+      value: pageSize,
+    })
 
     return (
-      <div
-        css={[paginationContainer, applyBoxStyle(props)]}
-        placeholder={placeholder}
-        ref={ref}
-        {...deleteCssProps(otherProps)}
-      >
-        {totalElement}
-        {simple ? (
-          <SimplePagination
-            defaultSelectedPageIndex={curPage}
-            pageSum={pageSum}
-            size={size}
-            onCurrentChange={(pageNum) => {
-              if (pageNum == 0) return
-              changeCurrentPage(pageNum - 1)
-              onChange?.(pageNum - 1, itemCount)
-            }}
-            nextIcon={_nextIcon}
-            prevIcon={_preIcon}
-            wholeDisabled={disabled == true}
-          />
-        ) : (
-          <span css={paginationContainer}>
-            <span
-              css={applyDefaultItemWithMarginCss(size, prevDisable)}
-              onClick={() => {
-                if (prevDisable) return
-                changeCurrentPage(curPage - 1)
-                onChange?.(curPage - 1, itemCount)
-              }}
-            >
-              {_preIcon}
-            </span>
-            {pageSum > 0 && (
-              <PageNumGroup
-                total={pageSum}
-                moreIcon={_moreIcon}
-                selectedIndex={curPage}
-                wholeDisable={disabled}
-                size={size}
-                onCurPageIndexChanged={(index) => {
-                  changeCurrentPage(index)
-                  onChange?.(index, itemCount)
-                }}
-              />
-            )}
-            <span
-              css={applyDefaultItemCss(size, nextDisable)}
-              onClick={() => {
-                if (nextDisable) return
-                changeCurrentPage(curPage + 1)
-                onChange?.(curPage + 1, itemCount)
-              }}
-            >
-              {_nextIcon}
-            </span>
+      <div css={paginationContainer}>
+        {showTotal && (
+          <span css={totalTextStyle}>
+            {locale.total.replace("{0}", total.toString())}
           </span>
         )}
-
-        {!simple && showJumper && (
-          <JumperInput
-            currentPage={curPage}
-            totalPages={pageSum}
-            wholeDisable={disabled}
-            size={size}
-            onEnterPress={(pageNum) => {
-              if (pageNum > pageSum - 1) {
-                changeCurrentPage(pageSum - 1)
-                onChange?.(pageSum - 1, itemCount)
-              } else {
-                changeCurrentPage(pageNum - 1)
-                onChange?.(pageNum - 1, itemCount)
-              }
-            }}
-          />
-        )}
-        {!simple && sizeCanChange && (
-          <PageSizeSelector
-            sizeOptions={sizeOptions}
-            wholeDisable={disabled}
-            size={size}
-            onPageSizeSelected={(pageSize) => {
-              changePageSize(pageSize)
-              changeCurrentPage(0)
-              onPageSizeChange?.(pageSize, 0)
-              onChange?.(0, pageSize)
-            }}
-          />
+        {showJumper && (
+          <div css={jumperContainerStyle}>
+            <span css={totalTextStyle}>{locale.go}</span>
+            <Input variant="fill" w="32px" h="32px" ml="16px" />
+          </div>
         )}
       </div>
     )
