4ad37f1f90f95e4e27f8ceb5dcd5d449c36d585a
shandamengcheng <zhumengcheng@illasoft.com>
2022-12-13 17:59:01 +0800

    feat: add countup animation

--- packages/statistic/stories/statistic.stories.tsx
+++ packages/statistic/stories/statistic.stories.tsx
@@ -1,6 +1,7 @@
 import { Meta, Story } from "@storybook/react"
 import { Statistic, StatisticProps } from "../src"
-import { Space, ImageDefaultIcon, UpIcon } from "@illa-design/react"
+import { Space, ImageDefaultIcon, UpIcon, Button } from "@illa-design/react"
+import React, { useRef } from "react"
 
 export default {
   title: "DATA DISPLAY/Statistic",
@@ -8,10 +9,29 @@ export default {
 } as Meta
 
 const Template: Story<StatisticProps> = (args) => {
+  let refGrowth = useRef<HTMLElement>(null)
+
   return (
     <Space size={"large"}>
       <Statistic {...args} />
       <Statistic {...args} title={<ImageDefaultIcon />} suffix={<UpIcon />} />
+      <Statistic
+        ref={(ref) => (refGrowth = ref)}
+        title="User Growth Rate"
+        value={123344}
+        precision={2}
+        prefix={<UpIcon />}
+        suffix="%"
+        countUp
+      />
+      <Button
+        onClick={() => {
+          refGrowth && refGrowth.onCountUp()
+        }}
+        style={{ display: "block", marginTop: 10 }}
+      >
+        Start
+      </Button>
     </Space>
   )
 }
