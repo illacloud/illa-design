ebf3296b06b392c66f052a66b963a50316d798ba
smallSohoSolo <b3069741@gmail.com>
2023-01-30 12:12:53 +0800

    fix: story preview type

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,8 +1,14 @@
 export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
+    },
+  },
+  docs: {
+    source: {
+      type: "code",
     },
   },
 }
