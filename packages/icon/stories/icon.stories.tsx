d1247d4717fc6467a1d8e17ee7e3b33cb6cc1f44
smallSohoSolo <b3069741@gmail.com>
2023-05-05 18:54:38 +0800

    feat: add refresh icon

--- packages/icon/stories/icon.stories.tsx
+++ packages/icon/stories/icon.stories.tsx
@@ -65,6 +65,7 @@ import {
   PreviousDoubleIcon,
   PreviousIcon,
   ReduceIcon,
+  RefreshIcon,
   ResetIcon,
   SearchIcon,
   SettingIcon,
@@ -278,6 +279,12 @@ export const All: StoryFn<IconProps> = (props: IconProps) => (
         <Space direction="vertical" align="center" w="100%" pd="16px 0">
           <DragPointIcon {...props} />
           <span>DragPoint</span>
+        </Space>
+      </Col>
+      <Col span={2}>
+        <Space direction="vertical" align="center" w="100%" pd="16px 0">
+          <RefreshIcon {...props} />
+          <span>Refresh</span>
         </Space>
       </Col>
     </Row>
