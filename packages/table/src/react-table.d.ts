2f5815b3705643a9396d4547057efccd37f8b118
xiaoyu <lavamint@outlook.com>
2023-05-06 15:17:23 +0800

    feat: update react-table.d.ts

--- packages/table/src/react-table.d.ts
+++ packages/table/src/react-table.d.ts
@@ -1,8 +1,9 @@
-import type { RowData } from '@tanstack/react-table';
-import '@tanstack/react-table'
+import type { RowData } from "@tanstack/react-table"
+import "@tanstack/react-table"
+import { SerializedStyles } from "@emotion/react"
 
-declare module '@tanstack/react-table' {
+declare module "@tanstack/react-table" {
   interface ColumnMeta<TData extends RowData, TValue> {
-    align?: 'right';
+    style?: SerializedStyles
   }
 }
