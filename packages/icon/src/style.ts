66cf2bf7bb0d6b35b6f17e7de6f5354a6b1ffb70
AruSeito <weichen@illasoft.com>
2023-05-17 16:55:28 +0800

    fix: 🐛 center Icon vertically by default

--- packages/icon/src/style.ts
+++ packages/icon/src/style.ts
@@ -12,3 +12,17 @@ export const rotateKeyframe = keyframes`
 export const rotateAnimation = css`
   animation: 1s linear infinite ${rotateKeyframe};
 `
+
+export const iconContainerStyle = css`
+  display: inline-flex;
+  align-items: center;
+  font-style: normal;
+  line-height: 0;
+  color: inherit;
+  text-align: center;
+  text-rendering: optimizeLegibility;
+  text-transform: none;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  vertical-align: -0.125em;
+`
