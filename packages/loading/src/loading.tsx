a25cc2a7ead4911223541ce61124492ceff2e0f4
smallSohoSolo <b3069741@gmail.com>
2023-08-24 18:33:20 +0800

    chore: update icon

--- packages/loading/src/loading.tsx
+++ packages/loading/src/loading.tsx
@@ -3,13 +3,14 @@ import { css } from "@emotion/react"
 import { LoadingProps } from "./interface"
 import { applyLoadingStyle } from "./style"
 import { applyBoxStyle, deleteCssProps } from "@illa-design/theme"
+import { LoadingIcon } from "@illa-design/icon"
 
 export const Loading = forwardRef<HTMLDivElement, LoadingProps>(
   (props, ref) => {
     const { colorScheme = "gray", size = "medium", ...restProps } = props
     return (
-      <div
-        ref={ref}
+      <LoadingIcon
+        spin={true}
         css={css(applyLoadingStyle(colorScheme, size), applyBoxStyle(props))}
         {...deleteCssProps(restProps)}
       />
