a68b75e7ea3bc239b519667f273a3015c06df977
AruSeito <weichen@illasoft.com>
2023-10-26 19:31:27 +0800

    feat: ✨ update icon

--- packages/icon/src/icons/caret-right.tsx
+++ packages/icon/src/icons/caret-right.tsx
@@ -3,14 +3,7 @@ import { createIcon } from "../create-icon"
 export const CaretRightIcon = createIcon({
   title: "CaretRightIcon",
   viewBox: "0 0 8 8",
-  path: (
-    <path
-      fillRule="evenodd"
-      clipRule="evenodd"
-      d="M1.954 6.727A.3.3 0 0 1 1.5 6.47V1.53a.3.3 0 0 1 .454-.257l3.974 2.384a.4.4 0 0 1 0 .686L1.954 6.727z"
-      fill="currentColor"
-    />
-  ),
+  path: <path d="M2.5 6V2L5.5 4L2.5 6Z" fill="currentColor" />,
 })
 
 CaretRightIcon.displayName = "CaretRightIcon"
