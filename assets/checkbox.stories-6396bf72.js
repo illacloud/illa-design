5f8c1d920b5f09712f3ae71361bacc07419bc7d6
AruSeito <AruSeito@users.noreply.github.com>
2023-11-08 08:09:31 +0000

    deploy: 0a75962506c9aab91362c1789b62b55385d90290

--- assets/checkbox.stories-6396bf72.js
+++
@@ -1,4 +0,0 @@
-import{a as i}from"./emotion-react-jsx-runtime.browser.esm-1facf72c.js";import{C as s}from"./checkbox-93d4b1ac.js";import"./index-1cdf6ce0.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-d9790902.js";import"./style-3ea54b04.js";import"./color-palette-728b424e.js";import"./index-45a08ae3.js";import"./reduce-2a80ac7e.js";import"./create-icon-907f0980.js";import"./success-eaf0bf13.js";import"./use-merge-value-fcf53e13.js";import"./is-47f46886.js";import"./use-previous-5c929b6e.js";const L={title:"DATA INPUT/Checkbox",component:s},r=m=>i(s,{...m,children:"ILLA"});var o,t,e;r.parameters={...r.parameters,docs:{...(o=r.parameters)==null?void 0:o.docs,source:{originalSource:`args => {
-  return <Checkbox {...args}>ILLA</Checkbox>;
-}`,...(e=(t=r.parameters)==null?void 0:t.docs)==null?void 0:e.source}}};const f=["Basic"];export{r as Basic,f as __namedExportsOrder,L as default};
-//# sourceMappingURL=checkbox.stories-6396bf72.js.map
---
+++ assets/select.stories-686306c4.js
@@ -0,0 +1,82 @@
+import{j as a,a as o}from"./emotion-react-jsx-runtime.browser.esm-1facf72c.js";import{r}from"./index-1cdf6ce0.js";import{S as t}from"./select-d5df451d.js";import{P as u}from"./people-0aeaca60.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-d9790902.js";import"./style-3ea54b04.js";import"./empty-aff91d97.js";import"./empty-3c50aa28.js";import"./create-icon-907f0980.js";import"./config-provider-context-e3e0b003.js";import"./image-c56aa3a3.js";import"./image-default-3cbff2d8.js";import"./input-7a42d9dd.js";import"./style-4ee563c0.js";import"./color-palette-728b424e.js";import"./index-45a08ae3.js";import"./z-index-efffafd0.js";import"./clear-bb423cf4.js";import"./use-merge-value-fcf53e13.js";import"./is-47f46886.js";import"./use-previous-5c929b6e.js";import"./up-1088cd79.js";import"./droplist-item-b7c2fc5b.js";import"./trigger-63b86d98.js";import"./trigger-context-e9ca2c5c.js";import"./index-a01a9712.js";import"./use-force-update-497a924b.js";import"./use-isomorphic-effect-1eadbd01.js";import"./is-browser-3ff01602.js";import"./SVGVisualElement-c2d20f19.js";import"./index-4cd6a639.js";import"./loading-be690f19.js";import"./down-3d8ebd18.js";import"./checkbox-1e093970.js";import"./reduce-2a80ac7e.js";import"./success-73b35753.js";import"./input-tag-c5073aa6.js";import"./web-cc1a3eed.js";import"./tag-e795cf83.js";import"./close-b3537d01.js";const X={title:"DATA INPUT/Select",component:t},e=n=>(r.useState("option2"),a("div",{children:[o(t,{value:1,options:[1,2,3],...n}),o(t,{mt:"20px",options:[1,2,3],multiple:!0,...n}),o(t,{...n,mt:"20px",value:"option1",options:[{label:"Option 1",value:"option1"},{label:"Option 2",value:"option2"},{label:"Option 3",value:"option3"}],showSearch:!0}),o(t,{mt:"20px",options:[{label:"Option 1",value:"option1"},{label:"Option 2",value:"option2"},{label:"Option 3",value:"option3"}],showSearch:!0,allowClear:!0,filterOption:!0,...n}),o(t,{mt:"20px",options:[{label:o(u,{}),value:"option1"},{label:"Option 2",value:"option2"},{label:"Option 3",value:"option3"}],...n}),o(t,{mt:"20px",multiple:!0,showSearch:!0,filterOption:!0,options:[{label:"Option 1",value:"option1"},{label:"Option 2",value:"option2"},{label:"Option 3",value:"option3"}],...n}),o(t,{...n,mt:"20px",multiple:!0,showSearch:!0,value:["option2"],filterOption:!0,options:[{label:"Option 1",value:"option1"},{label:"Option 2",value:"option2"},{label:"Option 3",value:"option3",disabled:!0}]}),o(t,{...n,mt:"20px",options:[{label:"Option 1",value:1},{label:"Option 2",value:2},{label:"Option 3",value:3}]}),o(t,{...n,mt:"20px",multiple:!0,options:[{label:"Option 1",value:1},{label:"Option 2",value:2},{label:"Option 3",value:3}]}),o(t,{mt:"20px",options:["option1","option2","option3"],...n}),o(t,{mt:"20px",multiple:!0,options:[1,2,3],...n}),o(t,{mt:"20px",options:[1,2,3],...n})]}));var p,l,i;e.parameters={...e.parameters,docs:{...(p=e.parameters)==null?void 0:p.docs,source:{originalSource:`args => {
+  const [currentValue, setCurrentValue] = useState("option2");
+  return <div>
+      <Select value={1} options={[1, 2, 3]} {...args} />
+      <Select mt="20px" options={[1, 2, 3]} multiple {...args} />
+      <Select {...args} mt="20px" value="option1" options={[{
+      label: "Option 1",
+      value: "option1"
+    }, {
+      label: "Option 2",
+      value: "option2"
+    }, {
+      label: "Option 3",
+      value: "option3"
+    }]} showSearch={true} />
+      <Select mt="20px" options={[{
+      label: "Option 1",
+      value: "option1"
+    }, {
+      label: "Option 2",
+      value: "option2"
+    }, {
+      label: "Option 3",
+      value: "option3"
+    }]} showSearch allowClear filterOption={true} {...args} />
+      <Select mt="20px" options={[{
+      label: <PeopleIcon />,
+      value: "option1"
+    }, {
+      label: "Option 2",
+      value: "option2"
+    }, {
+      label: "Option 3",
+      value: "option3"
+    }]} {...args} />
+      <Select mt="20px" multiple showSearch filterOption={true} options={[{
+      label: "Option 1",
+      value: "option1"
+    }, {
+      label: "Option 2",
+      value: "option2"
+    }, {
+      label: "Option 3",
+      value: "option3"
+    }]} {...args} />
+      <Select {...args} mt="20px" multiple showSearch value={["option2"]} filterOption={true} options={[{
+      label: "Option 1",
+      value: "option1"
+    }, {
+      label: "Option 2",
+      value: "option2"
+    }, {
+      label: "Option 3",
+      value: "option3",
+      disabled: true
+    }]} />
+      <Select {...args} mt="20px" options={[{
+      label: "Option 1",
+      value: 1
+    }, {
+      label: "Option 2",
+      value: 2
+    }, {
+      label: "Option 3",
+      value: 3
+    }]} />
+      <Select {...args} mt="20px" multiple options={[{
+      label: "Option 1",
+      value: 1
+    }, {
+      label: "Option 2",
+      value: 2
+    }, {
+      label: "Option 3",
+      value: 3
+    }]} />
+      <Select mt="20px" options={["option1", "option2", "option3"]} {...args} />
+      <Select mt="20px" multiple options={[1, 2, 3]} {...args} />
+      <Select mt="20px" options={[1, 2, 3]} {...args} />
+    </div>;
+}`,...(i=(l=e.parameters)==null?void 0:l.docs)==null?void 0:i.source}}};const Y=["Basic"];export{e as Basic,Y as __namedExportsOrder,X as default};
+//# sourceMappingURL=select.stories-686306c4.js.map
