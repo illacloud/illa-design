1b8a7d3e4633f374e3f77a3ac24809b442199707
Wangtaofeng <Wangtaofeng@users.noreply.github.com>
2023-12-12 02:39:37 +0000

    deploy: 468d5572172739049733dc489fd4d846ac5d4279

---
+++ assets/image.stories-9d8d312a.js
@@ -0,0 +1,15 @@
+import{j as i,a as t}from"./emotion-react-jsx-runtime.browser.esm-1facf72c.js";import{a as n}from"./index.esm-52d37734.js";import{I as e}from"./image-f3e173be.js";import"./index-1cdf6ce0.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-d9790902.js";import"./iconBase-4112cc44.js";import"./style-4011d251.js";import"./image-default-69467f7d.js";import"./create-icon-e804097a.js";const k={title:"General/Image",component:e,argTypes:{objectFit:{options:["fill","container","cover","none","scale-down"],control:{type:"select"}},alt:{control:{type:"text"}},fallbackSrc:{control:{type:"text"}},src:{control:{type:"text"}},width:{control:{type:"text"}},height:{control:{type:"text"}},radius:{control:{type:"text"}},fallback:{control:!1}}},p=o=>i("div",{children:[t(e,{...o}),t(e,{style:{marginLeft:"10px"},src:"https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",...o}),t(e,{style:{marginLeft:"10px"},src:"empty",fallbackSrc:"https://media.istockphoto.com/photos/exclamation-point-picture-id1300306784?b=1&k=20&m=1300306784&s=170667a&w=0&h=zARxLkZL09BrdPnLT3xu4Ib6YLkJ8B5YlcGke7GqJ0g=",...o}),t(e,{style:{marginLeft:"10px"},src:"empty",fallback:t(n,{}),...o})]}),a=p.bind({});var r,s,c;a.parameters={...a.parameters,docs:{...(r=a.parameters)==null?void 0:r.docs,source:{originalSource:`args => {
+  return <div>
+      <Image {...args} />
+      <Image style={{
+      marginLeft: "10px"
+    }} src="https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png" {...args} />
+      <Image style={{
+      marginLeft: "10px"
+    }} src="empty" fallbackSrc="https://media.istockphoto.com/photos/exclamation-point-picture-id1300306784?b=1&k=20&m=1300306784&s=170667a&w=0&h=zARxLkZL09BrdPnLT3xu4Ib6YLkJ8B5YlcGke7GqJ0g=" {...args} />
+      <Image style={{
+      marginLeft: "10px"
+    }} src="empty" fallback={<BsTwitch />} {...args} />
+    </div>;
+}`,...(c=(s=a.parameters)==null?void 0:s.docs)==null?void 0:c.source}}};const y=["Basic"];export{a as Basic,y as __namedExportsOrder,k as default};
+//# sourceMappingURL=image.stories-9d8d312a.js.map
