efbd11519c625f5659fcfab2f88c69eacf5132ea
Scenery <1507337624@qq.com>
2023-12-20 17:26:30 +0800

    fix: number input ref bug

--- packages/input-number/src/input-number.tsx
+++ packages/input-number/src/input-number.tsx
@@ -1,9 +1,15 @@
-import { forwardRef, MutableRefObject, useCallback, useRef } from "react"
+import {
+  forwardRef,
+  MutableRefObject,
+  useCallback,
+  useImperativeHandle,
+  useRef,
+} from "react"
 import { InputNumberProps } from "./interface"
 import { Input } from "@illa-design/input"
 import { DownIcon, MinusIcon, PlusIcon, UpIcon } from "@illa-design/icon"
 import { Space } from "@illa-design/space"
-import { isNumber, useMergeValue } from "@illa-design/system"
+import { isNumber, mergeRefs, useMergeValue } from "@illa-design/system"
 import {
   applyActionIconStyle,
   applyControlBlockStyle,
@@ -33,6 +39,7 @@ export const InputNumber = forwardRef<HTMLInputElement, InputNumberProps>(
       defaultValue,
       value,
       icons,
+      inputRef,
       formatter,
       parser,
       onChange,
@@ -44,8 +51,15 @@ export const InputNumber = forwardRef<HTMLInputElement, InputNumberProps>(
       defaultValue,
     })
 
-    const inputRef =
+    const currentInputRef =
       useRef<HTMLInputElement>() as MutableRefObject<HTMLInputElement>
+
+    // useImperativeHandle(inputRef, () => ({
+    //   ...currentInputRef.current,
+    //   focus: () => {
+    //     currentInputRef.current.focus()
+    //   },
+    // }))
 
     const plusStep = useCallback((): void => {
       const currentNumber = Number(finalValue)
@@ -103,7 +117,7 @@ export const InputNumber = forwardRef<HTMLInputElement, InputNumberProps>(
         <div
           css={applyControlBlockStyle("up", size)}
           onClick={() => {
-            inputRef.current.focus()
+            currentInputRef.current.focus()
             plusStep()
           }}
         >
@@ -112,7 +126,7 @@ export const InputNumber = forwardRef<HTMLInputElement, InputNumberProps>(
         <div
           css={applyControlBlockStyle("bottom", size)}
           onClick={() => {
-            inputRef.current.focus()
+            currentInputRef.current.focus()
             minusStep()
           }}
         >
@@ -148,7 +162,7 @@ export const InputNumber = forwardRef<HTMLInputElement, InputNumberProps>(
     return (
       <Input
         ref={ref}
-        inputRef={inputRef}
+        inputRef={mergeRefs(currentInputRef, inputRef)}
         _css={hoverControlStyle}
         size={size}
         value={finalValue}
@@ -167,8 +181,8 @@ export const InputNumber = forwardRef<HTMLInputElement, InputNumberProps>(
         }}
         onPressEnter={() => {
           const rawValue = parser
-            ? parser(inputRef.current.value)
-            : inputRef.current.value
+            ? parser(currentInputRef.current.value)
+            : currentInputRef.current.value
 
           const dealNum = dealNumber(rawValue)
 
