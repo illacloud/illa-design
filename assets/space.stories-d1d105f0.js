91a29bb481ea41a9f69468d9dbde552ac46fb021
smallSohoSolo <smallSohoSolo@users.noreply.github.com>
2023-12-25 11:08:21 +0000

    deploy: ea71423715a08fc245ba89dfbb47f768d2718a3b

---
+++ assets/space.stories-d1d105f0.js
@@ -0,0 +1,11 @@
+import{j as n,a as r}from"./emotion-react-jsx-runtime.browser.esm-1facf72c.js";import{S as p}from"./space-fd259cd6.js";import{T as a}from"./tag-d04a7fc3.js";import"./index-1cdf6ce0.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-d9790902.js";import"./style-4011d251.js";import"./divider-678e568f.js";import"./color-palette-83e58897.js";import"./index-45a08ae3.js";import"./close-56bf4497.js";import"./create-icon-e804097a.js";const j={title:"Layout/Space",component:p},c=i=>n(p,{...i,style:{width:"300px"},children:[r(a,{children:"B"}),r(a,{children:"我"}),r(a,{children:"English"}),r(a,{children:"开"})]}),e=c.bind({});var t,o,s;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`props => {
+  return <Space {...props} style={{
+    width: "300px"
+  }}>
+      <Tag>B</Tag>
+      <Tag>我</Tag>
+      <Tag>English</Tag>
+      <Tag>开</Tag>
+    </Space>;
+}`,...(s=(o=e.parameters)==null?void 0:o.docs)==null?void 0:s.source}}};const y=["Basic"];export{e as Basic,y as __namedExportsOrder,j as default};
+//# sourceMappingURL=space.stories-d1d105f0.js.map
---
+++ assets/trigger.stories-793a5cde.js
@@ -0,0 +1,63 @@
+import{a as t,j as i}from"./emotion-react-jsx-runtime.browser.esm-1facf72c.js";import{T as n}from"./trigger-54fa4cde.js";import{S as e}from"./space-fd259cd6.js";import{B as r}from"./button-group-context-7d794e03.js";import{I as g}from"./input-e0ce1352.js";import"./index-1cdf6ce0.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-d9790902.js";import"./style-4011d251.js";import"./color-palette-83e58897.js";import"./index-45a08ae3.js";import"./create-icon-e804097a.js";import"./trigger-context-e9ca2c5c.js";import"./index-a01a9712.js";import"./use-force-update-497a924b.js";import"./use-isomorphic-effect-1eadbd01.js";import"./is-browser-3ff01602.js";import"./SVGVisualElement-c2d20f19.js";import"./index-4cd6a639.js";import"./divider-678e568f.js";import"./loading-9fdd5a3f.js";import"./style-339eb169.js";import"./z-index-efffafd0.js";import"./clear-c3d7638a.js";import"./use-merge-value-fcf53e13.js";import"./is-47f46886.js";import"./use-previous-5c929b6e.js";const _={title:"DATA DISPLAY/Trigger",component:n,argTypes:{colorScheme:{options:["white","blackAlpha","gray","red","orange","yellow","green","blue","cyan","purple","grayBlue"],control:{type:"select"}}}},c=o=>t("div",{style:{height:500,overflow:"scroll",padding:100},children:i(e,{direction:"vertical",children:[i(e,{style:{marginLeft:70},children:[t(n,{...o,position:"top-start",content:"TopLeft",children:t(r,{children:"TL"})}),t(n,{...o,position:"top",content:"TOP",children:t(r,{children:"TOP"})}),t(n,{...o,position:"top-end",content:"TR",children:t(r,{children:"TR"})})]}),i(e,{direction:"horizontal",size:"200px",children:[i(e,{direction:"vertical",children:[t(n,{...o,position:"left-start",content:"LT",children:t(r,{children:"LT"})}),t(n,{...o,position:"left",content:"LEFT",children:t(r,{children:"LEFT"})}),t(n,{...o,position:"left-end",content:"LB",children:t(r,{children:"LB"})})]}),i(e,{direction:"vertical",style:{marginLeft:20},children:[t(n,{...o,position:"right-start",content:"RT",children:t(r,{children:"RT"})}),t(n,{...o,position:"right",content:"RIGHT",children:t(r,{children:"RIGHT"})}),t(n,{...o,position:"right-end",content:"RB",children:t(r,{children:"RB"})})]})]}),i(e,{style:{marginLeft:70},children:[t(n,{...o,position:"bottom-start",content:"BL",children:t(r,{onClick:l=>{},children:"BL"})}),t(n,{...o,position:"bottom",content:"Test",children:t(r,{children:"BOTTOM"})}),t(n,{...o,position:"bottom-end",trigger:"focus",content:"A design is a plan or specification for the construction of an object or system or for the implementation of an activity or process, or the result of that plan or specification in the form of a prototype, product or pro",children:t(g,{})})]})]})});var p,s,a;c.parameters={...c.parameters,docs:{...(p=c.parameters)==null?void 0:p.docs,source:{originalSource:`args => {
+  return <div style={{
+    height: 500,
+    overflow: "scroll",
+    padding: 100
+  }}>
+      <Space direction="vertical">
+        <Space style={{
+        marginLeft: 70
+      }}>
+          <Trigger {...args} position="top-start" content="TopLeft">
+            <Button>TL</Button>
+          </Trigger>
+          <Trigger {...args} position="top" content="TOP">
+            <Button>TOP</Button>
+          </Trigger>
+          <Trigger {...args} position="top-end" content="TR">
+            <Button>TR</Button>
+          </Trigger>
+        </Space>
+        <Space direction="horizontal" size="200px">
+          <Space direction="vertical">
+            <Trigger {...args} position="left-start" content="LT">
+              <Button>LT</Button>
+            </Trigger>
+            <Trigger {...args} position="left" content="LEFT">
+              <Button>LEFT</Button>
+            </Trigger>
+            <Trigger {...args} position="left-end" content="LB">
+              <Button>LB</Button>
+            </Trigger>
+          </Space>
+          <Space direction="vertical" style={{
+          marginLeft: 20
+        }}>
+            <Trigger {...args} position="right-start" content="RT">
+              <Button>RT</Button>
+            </Trigger>
+            <Trigger {...args} position="right" content="RIGHT">
+              <Button>RIGHT</Button>
+            </Trigger>
+            <Trigger {...args} position="right-end" content="RB">
+              <Button>RB</Button>
+            </Trigger>
+          </Space>
+        </Space>
+        <Space style={{
+        marginLeft: 70
+      }}>
+          <Trigger {...args} position="bottom-start" content="BL">
+            <Button onClick={ignore => {}}>BL</Button>
+          </Trigger>
+          <Trigger {...args} position="bottom" content="Test">
+            <Button>BOTTOM</Button>
+          </Trigger>
+          <Trigger {...args} position="bottom-end" trigger="focus" content="A design is a plan or specification for the construction of an object or system or for the implementation of an activity or process, or the result of that plan or specification in the form of a prototype, product or pro">
+            <Input />
+          </Trigger>
+        </Space>
+      </Space>
+    </div>;
+}`,...(a=(s=c.parameters)==null?void 0:s.docs)==null?void 0:a.source}}};const C=["Basic"];export{c as Basic,C as __namedExportsOrder,_ as default};
+//# sourceMappingURL=trigger.stories-793a5cde.js.map
