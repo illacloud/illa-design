a9d2490e70d25bd9e3f862bfa747a398e271a3ac
Wangtaofeng <Wangtaofeng@users.noreply.github.com>
2023-12-25 12:04:23 +0000

    deploy: 6d6d8c8427a623633798ac937a60699b99e793f7

---
+++ assets/button.stories-14339224.js
@@ -0,0 +1,29 @@
+import{j as c,a as t}from"./emotion-react-jsx-runtime.browser.esm-1facf72c.js";import{B as e}from"./index.esm-52d37734.js";import{B as o}from"./button-group-context-25344542.js";import{S as s}from"./space-29f127a3.js";import"./index-1cdf6ce0.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-d9790902.js";import"./iconBase-4112cc44.js";import"./style-4011d251.js";import"./color-palette-83e58897.js";import"./index-45a08ae3.js";import"./loading-9fdd5a3f.js";import"./create-icon-e804097a.js";import"./divider-90977a79.js";const k={title:"General/Button",component:o,argTypes:{leftIcon:{control:!1},rightIcon:{control:!1}}},r=n=>c(s,{direction:"vertical",align:"start",wrap:!0,children:[t(o,{...n,children:"Hello"}),t(o,{...n,leftIcon:t(e,{}),children:"Hello"}),t(o,{...n,rightIcon:t(e,{}),children:"Hello"}),t(o,{...n,leftIcon:t(e,{}),variant:"outline",children:"Hello"}),t(o,{...n,loading:!0,loadingText:"loading text",children:"Hello"}),t(o,{...n,loading:!0,children:"Hello"}),t(o,{...n,leftIcon:t(e,{})}),t(o,{...n,loading:!0}),t(o,{...n,disabled:!0,children:"Disable"}),t(o,{...n,variant:"text",children:"Text"})]});var l,a,i;r.parameters={...r.parameters,docs:{...(l=r.parameters)==null?void 0:l.docs,source:{originalSource:`args => {
+  return <Space direction="vertical" align="start" wrap>
+      <Button {...args}>Hello</Button>
+      <Button {...args} leftIcon={<BsFacebook />}>
+        Hello
+      </Button>
+      <Button {...args} rightIcon={<BsFacebook />}>
+        Hello
+      </Button>
+      <Button {...args} leftIcon={<BsFacebook />} variant="outline">
+        Hello
+      </Button>
+      <Button {...args} loading loadingText="loading text">
+        Hello
+      </Button>
+      <Button {...args} loading>
+        Hello
+      </Button>
+      <Button {...args} leftIcon={<BsFacebook />} />
+      <Button {...args} loading />
+      <Button {...args} disabled={true}>
+        Disable
+      </Button>
+      <Button {...args} variant="text">
+        Text
+      </Button>
+    </Space>;
+}`,...(i=(a=r.parameters)==null?void 0:a.docs)==null?void 0:i.source}}};const F=["Basic"];export{r as Basic,F as __namedExportsOrder,k as default};
+//# sourceMappingURL=button.stories-14339224.js.map
