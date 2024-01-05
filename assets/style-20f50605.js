a16b0069d7c7abb921ae7efcc023bebcd8f934f6
Wangtaofeng <Wangtaofeng@users.noreply.github.com>
2024-01-05 08:37:06 +0000

    deploy: a4e9544f5d9c60f175e87fc37dd0e69b1d3f1048

---
+++ assets/style-20f50605.js
@@ -0,0 +1,26 @@
+import{c as i}from"./style-4011d251.js";import{a as o,g as e}from"./color-palette-83e58897.js";const a=t=>t==="white"?e("white","02"):t==="blackAlpha"||t==="gray"||t==="grayBlue"?e(t,"04"):e(t,"05"),g=i`
+  display: inline-block;
+  line-height: 1.33;
+  font-size: 12px;
+  font-weight: 400;
+`,y=t=>i`
+  margin-bottom: 4px;
+  color: ${a(t)};
+`,f=t=>i`
+  font-size: 12px;
+  margin-top: 4px;
+  color: ${a(t)};
+`,m=t=>i`
+  font-size: 24px;
+  font-weight: 500;
+  display: flex;
+  align-items: baseline;
+  color: ${o(t)};
+`;function x(t,s){const n=s?4:8,l=t?`margin-right:${n}px;`:`margin-left:${n}px;`;return i`
+    display: inline-block;
+    ${l+(s?"font-size: 14px;":"font-size: 18px;")}
+  `}const S=i`
+  display: inline-block;
+  direction: ltr;
+`;export{y as a,m as b,x as c,S as d,f as e,g as s};
+//# sourceMappingURL=style-20f50605.js.map
