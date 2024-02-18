b3b14a9f53d04381c4a132e67ab5b7ba3da93e30
AruSeito <AruSeito@users.noreply.github.com>
2024-02-18 10:55:06 +0000

    deploy: 46942aa5dca8810a8c3666f8c95d064c2befa9f1

---
+++ assets/dropdown.stories-bb110a50.js
@@ -0,0 +1,18 @@
+import{j as m,a as t}from"./emotion-react-jsx-runtime.browser.esm-1facf72c.js";import{D as e,a as r,b as l}from"./droplist-item-3244616a.js";import{B as d}from"./button-group-context-9000c136.js";import{S as c}from"./space-0de4b3ea.js";import"./index-1cdf6ce0.js";import"./emotion-use-insertion-effect-with-fallbacks.browser.esm-d9790902.js";import"./use-merge-value-fcf53e13.js";import"./is-47f46886.js";import"./use-previous-5c929b6e.js";import"./trigger-54fa4cde.js";import"./style-4011d251.js";import"./color-palette-83e58897.js";import"./index-45a08ae3.js";import"./create-icon-e804097a.js";import"./trigger-context-e9ca2c5c.js";import"./index-a01a9712.js";import"./use-force-update-497a924b.js";import"./use-isomorphic-effect-1eadbd01.js";import"./is-browser-3ff01602.js";import"./SVGVisualElement-c2d20f19.js";import"./index-4cd6a639.js";import"./loading-9fdd5a3f.js";import"./divider-f348b99a.js";const O={title:"NAVIGATION/Dropdown",component:e,argTypes:{icon:{control:!1}}},n=()=>m(l,{children:[t(r,{value:"blog",title:"Blog",disabled:!0},"1"),t(r,{value:"tutorial",title:"Tutorial"},"2"),t(r,{value:"docs",title:"Docs",selected:!0},"3"),t(r,{value:"community",title:"Community"},"4"),t(r,{value:"github",title:"Github"},"5")]}),o=i=>m(c,{children:[t(e,{dropList:n(),trigger:"hover",...i,children:t(d,{children:"Hover me"})}),t(e,{dropList:n(),trigger:"contextmenu",...i,children:t("div",{style:{display:"flex",alignItems:"center",justifyContent:"center",backgroundColor:"lightgray",width:200,height:150},children:"Click me on right"})})]});var s,p,a;o.parameters={...o.parameters,docs:{...(s=o.parameters)==null?void 0:s.docs,source:{originalSource:`args => <Space>
+    <Dropdown dropList={dropList()} trigger="hover" {...args}>
+      <Button>Hover me</Button>
+    </Dropdown>
+    <Dropdown dropList={dropList()} trigger={"contextmenu"} {...args}>
+      <div style={{
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      backgroundColor: "lightgray",
+      width: 200,
+      height: 150
+    }}>
+        Click me on right
+      </div>
+    </Dropdown>
+  </Space>`,...(a=(p=o.parameters)==null?void 0:p.docs)==null?void 0:a.source}}};const E=["Basic"];export{o as Basic,E as __namedExportsOrder,O as default};
+//# sourceMappingURL=dropdown.stories-bb110a50.js.map
